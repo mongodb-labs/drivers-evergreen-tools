b98daa2f1f60510a4ad17f802374e67aea8ca898
Shane Harvey <shane.harvey@mongodb.com>
2017-02-13 12:42:45 -0800

    Allow virtualenv to use pre-installed system packages

--- .evergreen/run-orchestration.sh
+++ .evergreen/run-orchestration.sh
@@ -44,7 +44,7 @@ if [ -f venv/bin/activate ]; then
   . venv/bin/activate
 elif [ -f venv/Scripts/activate ]; then
   . venv/Scripts/activate
-elif virtualenv venv || python -m virtualenv venv; then
+elif virtualenv --system-site-packages venv || python -m virtualenv --system-site-packages venv; then
   if [ -f venv/bin/activate ]; then
     . venv/bin/activate
   elif [ -f venv/Scripts/activate ]; then
