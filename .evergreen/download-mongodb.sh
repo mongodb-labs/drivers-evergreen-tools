d55fac92263cf3ddb935a6bf77acaba69931751e
Bernie Hackett <bernie.hackett@gmail.com>
2018-04-16 12:39:33 -0700

    Fix RHEL 7 downloads for s390x

--- .evergreen/download-mongodb.sh
+++ .evergreen/download-mongodb.sh
@@ -78,7 +78,7 @@ get_mongodb_download_url_for ()
              MONGODB_26="https://fastdl.mongodb.org/sunos5/mongodb-sunos5-x86_64-${VERSION_26}.tgz"
              MONGODB_24="https://fastdl.mongodb.org/sunos5/mongodb-sunos5-x86_64-${VERSION_24}.tgz"
       ;;
-      linux-rhel-7.2-s390x)
+      linux-rhel-7*-s390x)
          MONGODB_LATEST="http://downloads.10gen.com/linux/mongodb-linux-s390x-enterprise-rhel72-latest.tgz"
              MONGODB_36="http://downloads.10gen.com/linux/mongodb-linux-s390x-enterprise-rhel72-${VERSION_36}.tgz"
              MONGODB_34="http://downloads.10gen.com/linux/mongodb-linux-s390x-enterprise-rhel72-${VERSION_34}.tgz"
