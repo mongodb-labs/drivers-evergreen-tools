e3d13dd34d12b317253f0936b04070f5f34be5d9
Shane Harvey <shnhrv@gmail.com>
2018-05-24 14:55:07 -0700

    Revert "Use Python 3 from the toolchain when available (#44)"

--- .evergreen/run-orchestration.sh
+++ .evergreen/run-orchestration.sh
@@ -45,12 +45,6 @@ export ORCHESTRATION_URL="http://localhost:8889/v1/${TOPOLOGY}s"
 echo From shell `date` > $MONGO_ORCHESTRATION_HOME/server.log
 
 cd "$MONGO_ORCHESTRATION_HOME"
-
-# Prefer using Python 3 from the toolchain over the default system python.
-PYTHON=$(command -v /opt/mongodbtoolchain/v2/bin/python3 || command -v python3 || command -v python)
-$PYTHON -c 'import sys; print(sys.version)'
-$PYTHON -c 'import ssl; print(ssl.OPENSSL_VERSION)'
-
 # Setup or use the existing virtualenv for mongo-orchestration.
 #
 # Many of the Linux distros in Evergreen ship Python 2.6 as the
@@ -65,7 +59,7 @@ if [ -f venv/bin/activate ]; then
   . venv/bin/activate
 elif [ -f venv/Scripts/activate ]; then
   . venv/Scripts/activate
-elif $PYTHON -m virtualenv --system-site-packages --no-download venv || $PYTHON -m virtualenv --system-site-packages venv || virtualenv --system-site-packages --no-download venv || virtualenv --system-site-packages venv; then
+elif python -m virtualenv --system-site-packages --no-download venv || python -m virtualenv --system-site-packages venv || virtualenv --system-site-packages --no-download venv || virtualenv --system-site-packages venv; then
   if [ -f venv/bin/activate ]; then
     . venv/bin/activate
   elif [ -f venv/Scripts/activate ]; then
