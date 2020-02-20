88a20f24fdeed81b129dad38b116e2c77c4abab3
Vincent Kam <vincent.kam@10gen.com>
2020-02-19 19:00:42 -0500

    Add support for OCSP delegates + ECDSA certs

--- .evergreen/ocsp/mock_ocsp_revoked.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-# Use the CA as the OCSP responder (see https://community.letsencrypt.org/t/unable-to-verify-ocsp-response/7264)
-python3 ocsp_mock.py \
-  --ca_file ca_ocsp.pem \
-  --ocsp_responder_cert ca_ocsp.crt \
-  --ocsp_responder_key ca_ocsp.key \
-   -p 8100 \
-   -v \
-  --fault revoked
