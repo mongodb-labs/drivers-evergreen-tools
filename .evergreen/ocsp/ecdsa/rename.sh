38151a4a8dafd5cf5db8887bfc1b1ce78b7b4978
Vincent Kam <vincent.kam@10gen.com>
2020-04-17 18:24:41 -0400

    DRIVERS-1216: Add single responder OCSP certificates to drivers-evergreen-tools

--- .evergreen/ocsp/ecdsa/rename.sh
+++ .evergreen/ocsp/ecdsa/rename.sh
@@ -4,5 +4,7 @@
 [ ! -f ecdsa-ca-ocsp.key ] ||  mv ecdsa-ca-ocsp.key ca.key
 [ ! -f ecdsa-server-ocsp.pem ] || mv ecdsa-server-ocsp.pem server.pem
 [ ! -f ecdsa-server-ocsp-mustStaple.pem ] || mv ecdsa-server-ocsp-mustStaple.pem server-mustStaple.pem
+[ ! -f ecdsa-server-ocsp-singleEndpoint.pem ] || mv ecdsa-server-ocsp-singleEndpoint.pem server-singleEndpoint.pem
+[ ! -f ecdsa-server-ocsp-mustStaple-singleEndpoint.pem ] || mv ecdsa-server-ocsp-mustStaple-singleEndpoint.pem server-mustStaple-singleEndpoint.pem
 [ ! -f ecdsa-ocsp-responder.crt ] || mv ecdsa-ocsp-responder.crt ocsp-responder.crt
 [ ! -f ecdsa-ocsp-responder.key ] || mv ecdsa-ocsp-responder.key ocsp-responder.key
