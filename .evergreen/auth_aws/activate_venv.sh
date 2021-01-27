830f4757e0a88b278bfefd711a0ab04a3ec12af0
emadum <eric.adum@mongodb.com>
2021-01-27 15:42:48 -0500

    fix: add scripts for using venv for aws-auth

---
+++ .evergreen/auth_aws/activate_venv.sh
@@ -0,0 +1,25 @@
+set +x
+
+if [ "Windows_NT" = "$OS" ]; then
+  PYTHON_BINARY=python.exe
+else
+  PYTHON_BINARY=python3
+fi
+
+# create venv on first run
+if [ ! -d authawsvenv ]; then
+   FIRST_RUN=1
+   virtualenv -p ${PYTHON_BINARY} authawsvenv
+fi
+
+# always activate venv
+if [ "Windows_NT" = "$OS" ]; then
+  . authawsvenv/Scripts/activate
+else
+  . authawsvenv/bin/activate
+fi
+
+# install dependencies on first run
+if [ -z $FIRST_RUN ]; then
+  pip install --upgrade boto3
+fi
