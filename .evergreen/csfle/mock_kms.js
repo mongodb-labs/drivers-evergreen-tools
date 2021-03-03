6595aee8fb1d458b78af9eea6d854c59d74c53c5
Benjamin Rewis <benji.rewis@mongodb.com>
2021-03-03 16:57:18 -0500

    SPEC-1791 Add KMS mock server

---
+++ .evergreen/csfle/mock_kms.js
@@ -0,0 +1,22 @@
+/**
+ * Start a KMS mock server using variables from kms_setup.json
+ */
+load("lib/kms_lib.js");
+
+// Directory containing certificates to reference for starting KMS server.
+const CERT_DIRECTORY = "../x509gen/"
+
+(function() {
+"use strict";
+
+const config = readSetupJson();
+
+const caFile = CERT_DIRECTORY + config["kms_ca_file"];
+const certFile = CERT_DIRECTORY + config["kms_cert_file"];
+
+const pythonCommand = getPython3Binary() +
+    ` -u lib/kms_http_server.py --ca_file ${caFile} --cert_file ${certFile}`;
+const ret = runShellCmdWithEnv(pythonCommand, _);
+
+assert.eq(ret, 0, "Failed to start kms mock server");
+}());
