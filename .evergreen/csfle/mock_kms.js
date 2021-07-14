1d74a13bbdfebeffb5af22d7beb699a90823d68d
Benjamin Rewis <benji.rewis@mongodb.com>
2021-07-14 15:21:36 -0400

    Allow setting port on mock_kms script

--- .evergreen/csfle/mock_kms.js
+++ .evergreen/csfle/mock_kms.js
@@ -1,5 +1,18 @@
 /**
- * Start a KMS mock server using variables from kms_setup.json
+ * Start a KMS mock server using variables from kms_setup.json.
+ *
+ * To run the KMS mock server from this directory on port 8000 with a particular CA and cert file,
+ * use the following bash snippet.
+ *
+ *   cat <<EOF > kms_setup.json
+ *   {
+ *      "kms_ca_file": "path/to/ca_file",
+ *      "kms_cert_file": "path/to/cert_file",
+ *      "port": "8000"
+ *   }
+ *   EOF
+ *   mongo --nodb mock_kms.js
+ *
  */
 load("lib/kms_lib.js");
 
@@ -13,9 +26,10 @@ const config = readSetupJson();
 
 const caFile = CERT_DIRECTORY + config["kms_ca_file"];
 const certFile = CERT_DIRECTORY + config["kms_cert_file"];
+const port = config["port"];
 
 const pythonCommand = getPython3Binary() +
-    ` -u lib/kms_http_server.py --ca_file ${caFile} --cert_file ${certFile}`;
+    ` -u lib/kms_http_server.py --ca_file ${caFile} --cert_file ${certFile} --port ${port}`;
 const ret = runShellCmdWithEnv(pythonCommand, {});
 
 assert.eq(ret, 0, "Failed to start kms mock server");
