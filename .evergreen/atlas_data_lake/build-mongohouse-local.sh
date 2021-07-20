56414b8e7424a21bf5941f1d811f80eab1dba501
Eric Adum <eric.adum@mongodb.com>
2021-07-20 12:25:23 -0400

    update mongohouse build script paths (#164)

--- .evergreen/atlas_data_lake/build-mongohouse-local.sh
+++ .evergreen/atlas_data_lake/build-mongohouse-local.sh
@@ -35,11 +35,11 @@ GO111MODULE=on go mod download
 DL_END=$(date +%s)
 
 # Build mqlrun
-./cmd/buildscript/build.sh tools:download:mqlrun
+./build.sh tools:download:mqlrun
 export MONGOHOUSE_MQLRUN=`pwd`/artifacts/mqlrun
 
 # Build mongohouse
-./cmd/buildscript/build.sh build:mongohoused
+./build.sh build:mongohoused
 
 sleep 5
 
