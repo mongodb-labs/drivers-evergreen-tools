2746d4881399d000ba98e28a03ae064e907b93e3
Kevin Albertson <kevin.albertson@mongodb.com>
2021-10-14 09:59:41 -0400

    add pykmip.db with a SecretData registered with UID=1

--- .evergreen/csfle/kms_kmip_server.py
+++ .evergreen/csfle/kms_kmip_server.py
@@ -7,17 +7,21 @@ from kmip.services.server import KmipServer
 import os
 import logging
 
+HOSTNAME="localhost"
+PORT=5698
+
 def main():
     dir_path = os.path.dirname(os.path.realpath(__file__))
     drivers_evergreen_tools = os.path.join (dir_path, "../../")
 
     server = KmipServer(
-        hostname="localhost",
-        port=5698,
+        hostname=HOSTNAME,
+        port=PORT,
         certificate_path=os.path.join(drivers_evergreen_tools, ".evergreen/x509gen/server.pem"),
         ca_path=os.path.join(drivers_evergreen_tools, ".evergreen/x509gen/ca.pem"),
         config_path=None,
         log_path=os.path.join(drivers_evergreen_tools, ".evergreen/csfle/pykmip.log"),
+        database_path=os.path.join(drivers_evergreen_tools, ".evergreen/csfle/pykmip.db"),
         logging_level=logging.DEBUG,
     )
 
