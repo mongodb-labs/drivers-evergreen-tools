728c31bb41c89810c583e430da1f8010458610b1
Kevin Albertson <kevin.albertson@mongodb.com>
2021-11-03 16:00:22 -0400

    run autopep8

--- .evergreen/csfle/kms_kmip_server.py
+++ .evergreen/csfle/kms_kmip_server.py
@@ -7,26 +7,32 @@ from kmip.services.server import KmipServer
 import os
 import logging
 
-HOSTNAME="localhost"
-PORT=5698
+HOSTNAME = "localhost"
+PORT = 5698
+
 
 def main():
     dir_path = os.path.dirname(os.path.realpath(__file__))
-    drivers_evergreen_tools = os.path.join (dir_path, "..", "..")
+    drivers_evergreen_tools = os.path.join(dir_path, "..", "..")
     server = KmipServer(
         hostname=HOSTNAME,
         port=PORT,
-        certificate_path=os.path.join(drivers_evergreen_tools, ".evergreen", "x509gen", "server.pem"),
-        ca_path=os.path.join(drivers_evergreen_tools, ".evergreen", "x509gen", "ca.pem"),
+        certificate_path=os.path.join(
+            drivers_evergreen_tools, ".evergreen", "x509gen", "server.pem"),
+        ca_path=os.path.join(drivers_evergreen_tools,
+                             ".evergreen", "x509gen", "ca.pem"),
         config_path=None,
         auth_suite="TLS1.2",
-        log_path=os.path.join(drivers_evergreen_tools, ".evergreen", "csfle", "pykmip.log"),
-        database_path=os.path.join(drivers_evergreen_tools, ".evergreen", "csfle", "pykmip.db"),
+        log_path=os.path.join(drivers_evergreen_tools,
+                              ".evergreen", "csfle", "pykmip.log"),
+        database_path=os.path.join(
+            drivers_evergreen_tools, ".evergreen", "csfle", "pykmip.db"),
         logging_level=logging.DEBUG,
     )
     with server:
-        print ("Starting KMS KMIP server")
+        print("Starting KMS KMIP server")
         server.serve()
 
+
 if __name__ == "__main__":
-    main ()
+    main()
