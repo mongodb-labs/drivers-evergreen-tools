bd1363a78ecf3c47f0bffc7f088f92ff4071d2e3
Kevin Albertson <kevin.albertson@mongodb.com>
2021-11-03 18:14:14 -0400

    add arguments for ca_file, cert_file, and port

--- .evergreen/csfle/kms_kmip_server.py
+++ .evergreen/csfle/kms_kmip_server.py
@@ -6,6 +6,7 @@ KMS KMIP test server.
 from kmip.services.server import KmipServer
 import os
 import logging
+import argparse
 
 HOSTNAME = "localhost"
 PORT = 5698
@@ -14,13 +15,26 @@ PORT = 5698
 def main():
     dir_path = os.path.dirname(os.path.realpath(__file__))
     drivers_evergreen_tools = os.path.join(dir_path, "..", "..")
+    default_ca_file = os.path.join(
+        drivers_evergreen_tools, ".evergreen", "x509gen", "ca.pem")
+    default_cert_file = os.path.join(
+        drivers_evergreen_tools, ".evergreen", "x509gen", "server.pem")
+
+    parser = argparse.ArgumentParser(
+        description='MongoDB Mock KMIP KMS Endpoint.')
+    parser.add_argument('-p', '--port', type=int,
+                        default=PORT, help="Port to listen on")
+    parser.add_argument('--ca_file', type=str,
+                        default=default_ca_file, help="TLS CA PEM file")
+    parser.add_argument('--cert_file', type=str,
+                        default=default_cert_file, help="TLS Server PEM file")
+    args = parser.parse_args()
+
     server = KmipServer(
         hostname=HOSTNAME,
-        port=PORT,
-        certificate_path=os.path.join(
-            drivers_evergreen_tools, ".evergreen", "x509gen", "server.pem"),
-        ca_path=os.path.join(drivers_evergreen_tools,
-                             ".evergreen", "x509gen", "ca.pem"),
+        port=args.port,
+        certificate_path=args.cert_file,
+        ca_path=args.ca_file,
         config_path=None,
         auth_suite="TLS1.2",
         log_path=os.path.join(drivers_evergreen_tools,
@@ -30,7 +44,7 @@ def main():
         logging_level=logging.DEBUG,
     )
     with server:
-        print("Starting KMS KMIP server")
+        print(f"Starting KMS KMIP server on port {args.port}")
         server.serve()
 
 
