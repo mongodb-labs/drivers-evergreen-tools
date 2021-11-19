1941d9225197867242165651094eaa6e253337c7
Ezra Chung <ezra.chung@mongodb.com>
2021-11-19 13:35:25 -0600

    Pin cryptography to permit use of CRYPTOGRAPHY_DONT_BUILD_RUST

--- .evergreen/csfle/activate_venv.sh
+++ .evergreen/csfle/activate_venv.sh
@@ -25,5 +25,5 @@ fi
 
 # install dependencies on first run
 if [ ! -z $FIRST_RUN ]; then
-  pip install --upgrade boto3~=1.19 pykmip~=0.10.0
+  CRYPTOGRAPHY_DONT_BUILD_RUST=1 pip install --upgrade boto3~=1.19 cryptography~=3.4.8 pykmip~=0.10.0
 fi
