c5fd810d5f5f7b95c49a1fdda670f244062907ad
Ezra Chung <ezra.chung@mongodb.com>
2021-11-19 10:03:31 -0600

    Add server toolchain python3 to list of candidates

--- .evergreen/csfle/activate_venv.sh
+++ .evergreen/csfle/activate_venv.sh
@@ -2,14 +2,18 @@ if [ "Windows_NT" = "$OS" ]; then
   PYTHON_BINARY=C:/python/Python38/python.exe
 elif command -v /opt/python/3.6/bin/python3; then
   PYTHON_BINARY=/opt/python/3.6/bin/python3
-else
+elif command -v python3; then
   PYTHON_BINARY=python3
+elif command -v /opt/mongodbtoolchain/v2/bin/python3; then
+  PYTHON_BINARY=/opt/mongodbtoolchain/v2/bin/python3
+else
+  echo "error: unable to find a supported python3 executable"
 fi
 
 # create venv on first run
 if [ ! -d kmstlsvenv ]; then
    FIRST_RUN=1
-   virtualenv -p ${PYTHON_BINARY} kmstlsvenv
+   ${PYTHON_BINARY} -m virtualenv -p ${PYTHON_BINARY} kmstlsvenv
 fi
 
 # always activate venv
