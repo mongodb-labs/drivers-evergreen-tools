d9bf805ced93a255abf80eac6544c0407ab68e0b
Durran Jordan <durran@gmail.com>
2022-01-11 15:53:33 +0100

    feat(DRIVERS-1983): use set parameter for lb start

--- .evergreen/start-orchestration.sh
+++ .evergreen/start-orchestration.sh
@@ -40,7 +40,7 @@ elif $PYTHON -m virtualenv --system-site-packages --never-download venv || virtu
 fi
 
 # Install from github to get the latest mongo-orchestration.
-pip install --upgrade 'https://github.com/10gen/mongo-orchestration/archive/DRIVERS-1983.tar.gz'
+pip install --upgrade 'https://github.com/mongodb/mongo-orchestration/archive/master.tar.gz'
 pip list
 cd -
 
