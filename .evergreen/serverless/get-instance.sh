64c636094b08f2a786421359f180584240cfeef2
Jeremy Mikola <jmikola@gmail.com>
2022-04-19 16:56:30 -0400

    DRIVERS-1967: Serverless testing uses load balancer fronting single proxy
    Updates serverless scripts to use a multi-tenant MongoDB (MTM) deployment with the SINGLE_TARGET_SERVERLESS_DEPLOYMENT feature flag enabled (CLOUDP-117288).
    
    This simplifies much of the original logic since there is now only a single SRV URI. For BC, create-instance.sh still outputs all of the original expansions. Those can be cleaned up at a later date after all downstream drivers migrate to the new expansions.
    
    The create-instance.sh script was also updated to solicit an explicit instance name (e.g. generated within the Evergreen config).

--- .evergreen/serverless/get-instance.sh
+++ .evergreen/serverless/get-instance.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 set -o errexit
-set +o xtrace
+set +o xtrace # Disable xtrace to ensure credentials aren't leaked
+
+# Supported environment variables:
+#
+#   SERVERLESS_INSTANCE_NAME    Required. Serverless instance to query.
+#   SERVERLESS_DRIVERS_GROUP    Required. Atlas group for drivers testing.
+#   SERVERLESS_API_PUBLIC_KEY   Required. Public key for Atlas API request.
+#   SERVERLESS_API_PRIVATE_KEY  Required. Private key for Atlas API request.
 
 if [ -z "$SERVERLESS_INSTANCE_NAME" ]; then
     echo "Instance name must be provided via SERVERLESS_INSTANCE_NAME environment variable"
@@ -23,7 +30,8 @@ if [ -z "$SERVERLESS_API_PUBLIC_KEY" ]; then
     exit 1
 fi
 
-API_BASE_URL="https://account-dev.mongodb.com/api/private/nds/serverless/groups/$SERVERLESS_DRIVERS_GROUP"
+# See: https://www.mongodb.com/docs/atlas/reference/api/serverless/return-one-serverless-instance/
+API_BASE_URL="https://account-dev.mongodb.com/api/atlas/v1.0/groups/$SERVERLESS_DRIVERS_GROUP"
 
 curl \
   --silent \
@@ -32,6 +40,6 @@ curl \
   -X GET \
   --digest \
   --header "Accept: application/json" \
-  "${API_BASE_URL}/instances/${SERVERLESS_INSTANCE_NAME}?pretty=true" \
+  "${API_BASE_URL}/serverless/${SERVERLESS_INSTANCE_NAME}?pretty=true" \
 
 echo ""
