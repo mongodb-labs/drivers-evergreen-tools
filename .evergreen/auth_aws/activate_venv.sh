0413f76d6535c83175f114a44d20335a3412d08e
Shane Harvey <shnhrv@gmail.com>
2022-06-02 10:10:27 -0700

    DRIVERS-2346 Reduce redundancy

--- .evergreen/auth_aws/activate_venv.sh
+++ .evergreen/auth_aws/activate_venv.sh
@@ -6,26 +6,13 @@ else
   PYTHON_BINARY=python3
 fi
 
-# Get access to createvirtualenv.
+# Get access to createvenv.
 . "$(dirname "${BASH_SOURCE[0]}")/../utils.sh"
 
 # create venv on first run
 if [ ! -d authawsvenv ]; then
-   FIRST_RUN=1
-   createvirtualenv "$PYTHON_BINARY" authawsvenv
-fi
-
-# always activate venv
-if [ "Windows_NT" = "$OS" ]; then
-  # Workaround https://bugs.python.org/issue32451:
-  # authawsvenv/Scripts/activate: line 3: $'\r': command not found
-  dos2unix authawsvenv/Scripts/activate || true
-  . authawsvenv/Scripts/activate
-else
-  . authawsvenv/bin/activate
-fi
-
-# install dependencies on first run
-if [ ! -z $FIRST_RUN ]; then
+  createvenv "$PYTHON_BINARY" authawsvenv
   pip install --upgrade boto3
+else
+  activatevenv authawsvenv
 fi
