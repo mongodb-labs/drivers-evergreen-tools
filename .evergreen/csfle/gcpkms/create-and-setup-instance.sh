18399cea27ef4962d5e294a610030b7ad576c547
Kevin Albertson <kevin.albertson@mongodb.com>
2022-07-21 10:07:02 -0400

    add consolidated create-and-setup-instance.sh script

---
+++ .evergreen/csfle/gcpkms/create-and-setup-instance.sh
@@ -0,0 +1,30 @@
+# Create and setup a GCE instance.
+set -o errexit # Exit on first command error.
+if [ -z "$KEYFILE" -o -z "$DRIVERS_TOOLS" -o -z "$PROJECT" -o -z "$ZONE" -o -z "$SERVICEACCOUNT" ]; then
+    echo "Please set the following required environment variables"
+    echo " KEYFILE to the JSON file for the service account"
+    echo " DRIVERS_TOOLS to the path of the drivers-evergreen-tools directory"
+    echo " PROJECT to the GCP project"
+    echo " ZONE to the GCP zone"
+    echo " SERVICEACCOUNT to a GCP service account used to create and attach to the GCE instance"
+    exit 1
+fi
+
+# download-gcloud.sh sets GCLOUD.
+echo "download-gcloud.sh ... begin"
+. $DRIVERS_TOOLS/.evergreen/csfle/gcpkms/download-gcloud.sh
+echo "download-gcloud.sh ... end"
+
+$GCLOUD auth activate-service-account --key-file $KEYFILE
+
+# create-instance.sh sets INSTANCENAME.
+echo "create-instance.sh ... begin"
+. $DRIVERS_TOOLS/.evergreen/csfle/gcpkms/create-instance.sh
+echo "create-instance.sh ... end"
+
+echo "setup-instance.sh ... begin"
+. $DRIVERS_TOOLS/.evergreen/csfle/gcpkms/setup-instance.sh
+echo "setup-instance.sh ... end"
+
+echo "GCLOUD: $GCLOUD" > testgcpkms-expansions.yml
+echo "INSTANCENAME: $INSTANCENAME" >> testgcpkms-expansions.yml
