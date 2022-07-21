7fdd80b449ec87d615d75bd55e25c2e477ebfa12
Kevin Albertson <kevin.albertson@mongodb.com>
2022-07-21 10:19:27 -0400

    apply default PROJECT and ZONE

--- .evergreen/csfle/gcpkms/create-and-setup-instance.sh
+++ .evergreen/csfle/gcpkms/create-and-setup-instance.sh
@@ -1,16 +1,16 @@
 # Create and setup a GCE instance.
 set -o errexit # Exit on first command error.
-if [ -z "$KEYFILE" -o -z "$DRIVERS_TOOLS" -o -z "$PROJECT" -o -z "$ZONE" -o -z "$SERVICEACCOUNT" ]; then
+if [ -z "$KEYFILE" -o -z "$DRIVERS_TOOLS" -o -z "$SERVICEACCOUNT" ]; then
     echo "Please set the following required environment variables"
     echo " KEYFILE to the JSON file for the service account"
     echo " DRIVERS_TOOLS to the path of the drivers-evergreen-tools directory"
-    echo " PROJECT to the GCP project"
-    echo " ZONE to the GCP zone"
     echo " SERVICEACCOUNT to a GCP service account used to create and attach to the GCE instance"
     exit 1
 fi
 
 # Set defaults.
+export PROJECT=${PROJECT:-"csfle-poc"}
+export ZONE=${ZONE:-"us-east1-b"}
 export IMAGEPROJECT=${IMAGEPROJECT:-"debian-cloud"}
 export IMAGEFAMILY=${IMAGEFAMILY:-"debian-11"}
 export MACHINETYPE=${MACHINETYPE:-"e2-micro"}
