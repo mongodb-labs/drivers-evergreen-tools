40a06489f98e1f229af71e14f19242efae2e9aea
Kevin Albertson <kevin.albertson@mongodb.com>
2022-08-15 10:58:20 -0400

    CSHARP-4254 use pwd for download_and_extract_crypt_shared

--- .evergreen/download-mongodb.sh
+++ .evergreen/download-mongodb.sh
@@ -586,5 +586,5 @@ download_and_extract_crypt_shared ()
    rm -rf crypt_shared_download
 
    RELATIVE_CRYPT_SHARED_LIB_PATH="$(find . -maxdepth 1 -type f \( -name "$LIBRARY_NAME.dll" -o -name "$LIBRARY_NAME.so" -o -name "$LIBRARY_NAME.dylib" \))"
-   eval $__CRYPT_SHARED_LIB_PATH=$DRIVERS_TOOLS/../$(basename $RELATIVE_CRYPT_SHARED_LIB_PATH)
+   eval $__CRYPT_SHARED_LIB_PATH=$(pwd)/$(basename $RELATIVE_CRYPT_SHARED_LIB_PATH)
 }
