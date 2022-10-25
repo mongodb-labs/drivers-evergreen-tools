9676c844b3a803f9e401feb699f14c5ef280db54
Andreas Braun <git@alcaeus.org>
2022-10-25 11:51:08 +0200

    DRIVERS-2459: Add script to download mongocryptd and crypt_shared

---
+++ .evergreen/serverless/download-crypt.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+MONGODB_VERSION=${MONGODB_VERSION:-latest}
+
+DIR=$(dirname $0)
+# Functions to fetch MongoDB binaries
+. $DIR/../download-mongodb.sh
+
+get_distro
+get_mongodb_download_url_for "$DISTRO" "$MONGODB_VERSION"
+
+# Don't need to install the legacy shell
+download_and_extract_package "$MONGODB_DOWNLOAD_URL" "$EXTRACT"
+
+if [ -z $MONGO_CRYPT_SHARED_DOWNLOAD_URL ]; then
+  echo "There is no crypt_shared library for distro='$DISTRO' and version='$MONGODB_VERSION'".
+else
+  echo "Downloading crypt_shared package from $MONGO_CRYPT_SHARED_DOWNLOAD_URL"
+  download_and_extract_crypt_shared "$MONGO_CRYPT_SHARED_DOWNLOAD_URL" "$EXTRACT" CRYPT_SHARED_LIB_PATH
+  echo "CRYPT_SHARED_LIB_PATH:" $CRYPT_SHARED_LIB_PATH
+  if [ -z $CRYPT_SHARED_LIB_PATH ]; then
+    echo "CRYPT_SHARED_LIB_PATH must be assigned, but wasn't" 1>&2 # write to stderr"
+    exit 1
+  fi
+cat <<EOT >> serverless-expansion.yml
+CRYPT_SHARED_LIB_PATH: "$CRYPT_SHARED_LIB_PATH"
+EOT
+
+fi
