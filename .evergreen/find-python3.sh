f5d1c049948718218b006c0732990e1c642cbb13
Ezra Chung <88335979+eramongodb@users.noreply.github.com>
2022-11-03 16:17:51 -0500

    Add hygienic alternatives to Python virtual environment scripts (#236)
    * Add find-python3.sh
    
    * Add venv-utils.sh
    
    * Add activate-kmstlsvenv.sh

---
+++ .evergreen/find-python3.sh
@@ -0,0 +1,256 @@
+#!/usr/bin/env bash
+#
+# find-python3.sh
+#
+# Usage:
+#   . /path/to/find-python3.sh
+#
+# This file defines the following utility functions:
+#   - is_python3
+#   - is_venv_capable
+#   - is_virtualenv_capable
+#   - find_python3
+# These functions may be invoked from any working directory.
+
+# is_python3
+#
+# Usage:
+#   is_python3 python
+#   is_python3 /path/to/python
+#
+# Parameters:
+#   "$1": The name or path of the python binary to test.
+#
+# Return 0 (true) if the given argument "$1" is a Python 3 binary.
+# Return a non-zero value (false) otherwise.
+#
+# Diagnostic messages may be printed to stderr (pipe 2). Redirect to /dev/null
+# to silence these messages.
+is_python3() (
+  set -o errexit
+  set -o nounset
+  set -o pipefail
+
+  # Binary to use, e.g. "python".
+  local -r bin="${1:?'is_python3 requires a name or path of a python binary to test'}"
+
+  # Binary must be executable.
+  command -V "$bin" &>/dev/null || return
+
+  # Reject binaries that do not support argument "-V".
+  "$bin" -V &>/dev/null || return
+
+  # Expect an output of the form: "Python x.y.z".
+  # Note: Python 2 binaries output to stderr rather than stdout.
+  local -r version_output="$("$bin" -V 2>&1)"
+
+  # For diagnostic purposes.
+  echo " - $bin: $version_output"
+
+  # Evaluate result of this function.
+  # Note: Python True (1) and False (0) is treated as fail (1) and success (0)
+  # by Bash; therefore `is_python3` returns "true" when `v < 3` is false.
+  "$bin" -c "import sys; exit(sys.version_info[0] < 3)"
+) 1>&2
+
+# is_venv_capable
+#
+# Usage:
+#   is_venv_capable python
+#   is_venv_capable /path/to/python
+#
+# Parameters:
+#   "$1": The name or path of the python binary to test.
+#
+# Return 0 (true) if the given argument "$1" can successfully evaluate the
+# command:
+#   "$1" -m venv venv
+# and activate the created virtual environment.
+# Return a non-zero value (false) otherwise.
+#
+# Diagnostic messages may be printed to stderr (pipe 2). Redirect to /dev/null
+# to silence these messages.
+is_venv_capable() (
+  set -o errexit
+  set -o nounset
+  set -o pipefail
+
+  local -r bin="${1:?'is_venv_capable requires a name or path of a python binary to test'}"
+
+  # Use a temporary directory to avoid polluting the caller's environment.
+  local -r tmp="$(mktemp -d)"
+  trap 'rm -rf "$tmp"' EXIT
+
+  "$bin" -m venv "$tmp" || return
+
+  if [[ -f "$tmp/bin/activate" ]]; then
+    # shellcheck source=/dev/null
+    . "$tmp/bin/activate"
+  else
+    dos2unix "$tmp/Scripts/activate" || return
+    # shellcheck source=/dev/null
+    . "$tmp/Scripts/activate"
+  fi
+) 1>&2
+
+# is_virtualenv_capable
+#
+# Usage:
+#   is_virtualenv_capable python
+#   is_virtualenv_capable /path/to/python
+#
+# Parameters:
+#   "$1": The name or path of the python binary to test.
+#
+# Return 0 (true) if the given argument $1 can successfully evaluate the
+# command:
+#   "$1" -m virtualenv -p "$1" venv
+# and activate the created virtual environment.
+# Return a non-zero value (false) otherwise.
+#
+# Diagnostic messages may be printed to stderr (pipe 2). Redirect to /dev/null
+# to silence these messages.
+is_virtualenv_capable() (
+  set -o errexit
+  set -o nounset
+  set -o pipefail
+
+  local -r bin="${1:?'is_virtualenv_capable requires a name or path of a python binary to test'}"
+
+  # Use a temporary directory to avoid polluting the caller's environment.
+  local -r tmp="$(mktemp -d)"
+  trap 'rm -rf "$tmp"' EXIT
+
+  "$bin" -m virtualenv -p "$bin" "$tmp" || return
+
+  if [[ -f "$tmp/bin/activate" ]]; then
+    # shellcheck source=/dev/null
+    . "$tmp/bin/activate"
+  else
+    dos2unix "$tmp/Scripts/activate" || return
+    # shellcheck source=/dev/null
+    . "$tmp/Scripts/activate"
+  fi
+) 1>&2
+
+# find_python3
+#
+# Usage:
+#   find_python3
+#   PYTHON_BINARY=$(find_python3)
+#   PYTHON_BINARY=$(find_python3 2>/dev/null)
+#
+# Return 0 (true) if a Python 3 binary capable of creating a virtual environment
+# with either venv or virtualenv can be found.
+# Return a non-zero (false) value otherwise.
+#
+# If successful, print the name of the binary stdout (pipe 1).
+# Otherwise, no output is printed to stdout (pipe 1).
+#
+# Diagnostic messages may be printed to stderr (pipe 2). Redirect to /dev/null
+# with `2>/dev/null` to silence these messages.
+#
+# Example:
+#   PYTHON_BINARY=$(find_python3)
+#   if [[ -z "$PYTHON_BINARY" ]]; then
+#     # Handle missing Python binary situation.
+#   fi
+#
+#   if "$PYTHON_BINARY" -m venv -h; then
+#     "$PYTHON_BINARY" -m venv venv
+#   else
+#     "$PYTHON_BINARY" -m virtualenv -p "$PYTHON_BINARY" venv
+#   fi
+find_python3() (
+  set -o errexit
+  set -o nounset
+  set -o pipefail
+
+  local -a bins=()
+  local bin=""
+
+  # The list of Python binaries to test for venv or virtualenv support.
+  # The binaries are tested in the order of their position in the array.
+  {
+    echo "Finding python3 binaries to test..."
+
+    append_bins() {
+      local -r path="${1:?'missing path'}"
+      shift
+      local -r pattern="${1:?'missing pattern'}"
+      shift
+      local -ar suffixes=("${@:?'missing suffixes'}")
+
+      for dir in $(find "$path" -maxdepth 1 -name "$pattern" -type d 2>/dev/null | sort -rV); do
+        for bin in "${suffixes[@]}"; do
+          if is_python3 "$dir/$bin"; then
+            bins+=("$dir/$bin")
+          fi
+        done
+      done
+    }
+
+    # C:/python/Python3X/bin/python
+    append_bins "C:/python" "Python3[0-9]*" "python3.exe" "python.exe"
+
+    # /opt/python/3.X/bin/python
+    append_bins "/opt/python" "3.[0-9]*" "bin/python3" "bin/python"
+
+    # /opt/mongodbtoolchain/vX/bin/python
+    append_bins "/opt/mongodbtoolchain" "v[0-9]*" "bin/python3" "bin/python"
+
+    bin="python3"
+    if is_python3 "$bin"; then bins+=("$bin"); fi
+
+    bin="python"
+    if is_python3 "$bin"; then bins+=("$bin"); fi
+  } 1>&2
+
+  {
+    # Some environments trigger an unbound variable error if "${bins[@]}" is empty when used below.
+    if (("${#bins[@]}" == 0)); then
+      echo "Could not find any python3 binaries!"
+      return 1
+    fi
+
+    # For diagnostic purposes.
+    echo "List of python3 binaries to test:"
+    for bin in "${bins[@]}"; do
+      echo " - $bin"
+    done
+  } 1>&2
+
+  # Find a binary that is capable of venv or virtualenv and set it as `res`.
+  local res=""
+  {
+    echo "Testing python3 binaries..."
+    for bin in "${bins[@]}"; do
+      {
+        if ! is_venv_capable "$bin"; then
+          echo " - $bin is not capable of venv"
+
+          if ! is_virtualenv_capable "$bin"; then
+            echo " - $bin is not capable of virtualenv"
+            continue
+          else
+            echo " - $bin is capable of virtualenv"
+          fi
+        else
+          echo " - $bin is capable of venv"
+        fi
+      } 1>&2
+
+      res="$bin"
+      break
+    done
+
+    if [[ -z "$res" ]]; then
+      echo "Could not find a python3 binary capable of creating a virtual environment!"
+      return 1
+    fi
+  } 1>&2
+
+  echo "$res"
+
+  return 0
+)
---
+++ .evergreen/venv-utils.sh
@@ -0,0 +1,86 @@
+#!/usr/bin/env bash
+#
+# venv-utils.sh
+#
+# Usage:
+#   . /path/to/venv-utils.sh
+#
+# This file defines the following functions:
+#   - venvcreate
+#   - venvactivate
+# These functions may be invoked from any working directory.
+
+# venvcreate
+#
+# Usage:
+#   venvcreate python venv
+#   venvcreate /path/to/python /path/to/venv/dir
+#
+# Parameters:
+#   "$1": The Python binary to use for the virtual environment.
+#   "$2": The path to the virtual environment (directory) to create.
+#
+# Return 0 (true) if the virtual environment has been successfully created,
+# activated, and the pip package upgraded.
+# Return a non-zero value (false) otherwise.
+#
+# If a file or directory exists at the given path to the virtual environment,
+# they may be deleted as part of virtual environment creation.
+venvcreate() {
+  local -r bin="${1:?'venvcreate requires a Python binary to use for the virtual environment'}"
+  local -r path="${2:?'venvcreate requires a path to the virtual environment to create'}"
+
+  # Prefer venv, but fallback to virtualenv if venv fails.
+  for mod in "venv" "virtualenv"; do
+    # Ensure a clean directory before attempting to create a virtual environment.
+    rm -rf "$path"
+
+    if "$bin" -m "$mod" "$path"; then
+      # Workaround https://bugs.python.org/issue32451:
+      # mongovenv/Scripts/activate: line 3: $'\r': command not found
+      if [[ -f "$path/Scripts/activate" ]]; then
+        dos2unix "$path/Scripts/activate" || return
+      fi
+
+      if venvactivate "$path"; then
+        # Use --no-cache-dir to ensure ensure the *actual* latest pip is
+        # correctly installed.
+        if python -m pip install --no-cache-dir --upgrade pip; then
+          # Only consider success if activation + pip upgrade was successful.
+          return
+        fi
+
+        deactivate
+      fi
+    fi
+  done
+
+  echo "Could not use either venv or virtualenv with $bin to create a virtual environment at $path!" 1>&2
+  return 1
+}
+
+# venvactivate
+#
+# Usage:
+#   venvactivate venv
+#   venvactivate /path/to/venv/dir
+#
+# Parameters:
+#   "$1": The path to an existing virtual environment (directory).
+#
+# Activate the virtual environment "$1". Designed to work regardless of the
+# target environment.
+venvactivate() {
+  local -r path="${1:?'venvactivate requires a path to an existing virtual environment'}"
+
+  if [[ -f "$path/bin/activate" ]]; then
+    # shellcheck source=/dev/null
+    . "$path/bin/activate"
+  elif [[ -f "$path/Scripts/activate" ]]; then
+    # shellcheck source=/dev/null
+    . "$path/Scripts/activate"
+  else
+    echo "Could not find the script to activate the virtual environment at $path!" 1>&2
+    return 1
+  fi
+}
