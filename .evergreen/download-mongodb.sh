c225b2618bfb26d511e4c4df35d4022e37d26907
Andreas Braun <alcaeus@users.noreply.github.com>
2022-11-30 22:10:36 +0100

    DRIVERS-2492: Add MongoDB 5.0 back to RHEL 7.1-ppc (#249)

--- .evergreen/download-mongodb.sh
+++ .evergreen/download-mongodb.sh
@@ -162,6 +162,7 @@ get_mongodb_download_url_for ()
       ;;
       linux-rhel-7.1-ppc64le)
          MONGODB_LATEST="http://downloads.10gen.com/linux/mongodb-linux-ppc64le-enterprise-rhel71-latest.tgz"
+             MONGODB_50="http://downloads.10gen.com/linux/mongodb-linux-ppc64le-enterprise-rhel71-${VERSION_50}.tgz"
              MONGODB_44="http://downloads.10gen.com/linux/mongodb-linux-ppc64le-enterprise-rhel71-${VERSION_44}.tgz"
              MONGODB_42="http://downloads.10gen.com/linux/mongodb-linux-ppc64le-enterprise-rhel71-${VERSION_42}.tgz"
              MONGODB_40="http://downloads.10gen.com/linux/mongodb-linux-ppc64le-enterprise-rhel71-${VERSION_40}.tgz"
