8c308fc06d6cd585f962f04f620e998ae3d5b10b
Steven Silvester <steven.silvester@ieee.org>
2023-01-11 18:14:58 -0600

    remove unused call

--- .evergreen/auth_aws/aws_setup_oidc.js
+++ .evergreen/auth_aws/aws_setup_oidc.js
@@ -6,7 +6,6 @@
 
 const admin = Mongo().getDB("admin");
 assert(admin.auth("bob", "pwd123"));
-const config = readSetupJson();
 assert.commandWorked(admin.runCommand({createRole: 'test1/readWrite', roles:[{role: 'readWrite', db: "test"}], privileges: []}));
 assert.commandWorked(admin.runCommand({createRole: 'test2/read', roles:[{role: 'read', db: "test"}], privileges: []}));
 
