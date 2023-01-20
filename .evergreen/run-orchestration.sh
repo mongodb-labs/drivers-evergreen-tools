e1abe14d35ba40dbcfdadd46af75d279c945b1bf
Steven Silvester <steven.silvester@ieee.org>
2023-01-20 08:53:08 -0600

    undo change to run-orchestration

--- .evergreen/run-orchestration.sh
+++ .evergreen/run-orchestration.sh
@@ -36,7 +36,6 @@ else
   # Even though we have the MONGODB_DOWNLOAD_URL, we still call this to get the proper EXTRACT variable
   get_mongodb_download_url_for "$DISTRO"
 fi
-
 download_and_extract "$MONGODB_DOWNLOAD_URL" "$EXTRACT"
 
 DL_END=$(date +%s)
