fdec2c4d2baf53f3ada23d0dd79743a8f6b08c81
Steven Silvester <steven.silvester@ieee.org>
2023-01-31 09:05:55 -0600

    add script to get tokens

---
+++ .evergreen/auth_oidc/oidc_get_tokens.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+#
+# Bootstrapping file to launch a local oidc-enabled server and create
+# OIDC tokens that can be used for local testing.  See README for
+# prequisites and usage.
+#
+set -eux
+if [ -z "${AWS_ROLE_ARN}" ||  -z "${AWS_ACCESS_KEY_ID}" || -z "${AWS_SECRET_ACCESS_KEY}" ]; then
+    echo "Missing AWS credentials"
+    exit 1
+fi
+export AWS_TOKEN_DIR=${AWS_TOKEN_DIR:-/tmp/tokens}
+
+rm -rf authoidcvenv
+. ./activate_venv.sh
+python oidc_get_tokens.py
