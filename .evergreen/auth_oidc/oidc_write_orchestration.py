16aed8371d6b6ba36d405aefac15f60b776933ae
Steven Silvester <steven.silvester@ieee.org>
2023-02-06 10:02:49 -0600

    update parameter names

--- .evergreen/auth_oidc/oidc_write_orchestration.py
+++ .evergreen/auth_oidc/oidc_write_orchestration.py
@@ -21,8 +21,8 @@ def main():
 
     # Write the oidc orchestration file.
     provider1_info = {
-        "authURL": MOCK_ENDPOINT,
-        "tokenURL": MOCK_ENDPOINT,
+        "authorizationEndpoint": MOCK_ENDPOINT,
+        "tokenEndpoint": MOCK_ENDPOINT,
         "JWKSUri": secrets['oidc_jwks_uri'],
         "authNamePrefix": "test1",
         "issuer": secrets['oidc_issuer_1_uri'],
@@ -57,7 +57,7 @@ def main():
 
     provider1_info['matchPattern'] = "test_user1"
     provider2_info = {
-        "deviceAuthURL": MOCK_ENDPOINT,
+        "deviceAuthorizationEndpoint": MOCK_ENDPOINT,
         "JWKSUri": secrets['oidc_jwks_uri'],
         "authNamePrefix": "test2",
         "issuer": secrets['oidc_issuer_2_uri'],
