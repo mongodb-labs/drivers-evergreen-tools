79100c72a84bbfeca1430440fd9f5a34a14f3630
Steven Silvester <steven.silvester@ieee.org>
2023-02-10 10:10:38 -0600

    add a sleep before attempting to set up the server

--- .evergreen/auth_oidc/docker_entry.sh
+++ .evergreen/auth_oidc/docker_entry.sh
@@ -20,5 +20,6 @@ cd $DRIVERS_TOOLS/.evergreen/auth_oidc
 python oidc_write_orchestration.py
 
 bash $DRIVERS_TOOLS/.evergreen/run-orchestration.sh
+sleep 2
 $DRIVERS_TOOLS/mongodb/bin/mongo $DRIVERS_TOOLS/.evergreen/auth_oidc/setup_oidc.js
 tail -f $MONGO_ORCHESTRATION_HOME/server.log
