fb481f7689c2cf91b7a604de1a788d0a67516849
Julius Park <juliusgeo@gmail.com>
2023-02-14 15:34:36 -0800

    prevent globbing

--- .evergreen/run-orchestration.sh
+++ .evergreen/run-orchestration.sh
@@ -102,7 +102,7 @@ echo 'MONGODB_URI: "'$URI'"' > mo-expansion.yml
 echo $URI > $DRIVERS_TOOLS/uri.txt
 echo "Cluster URI: $URI"
 if [ -z "$SKIP_CRYPT_SHARED" ]; then
-  if [ -z $MONGO_CRYPT_SHARED_DOWNLOAD_URL ]; then
+  if [ -z "$MONGO_CRYPT_SHARED_DOWNLOAD_URL" ]; then
     echo "There is no crypt_shared library for distro='$DISTRO' and version='$MONGODB_VERSION'".
   else
     echo "Downloading crypt_shared package from $MONGO_CRYPT_SHARED_DOWNLOAD_URL"
