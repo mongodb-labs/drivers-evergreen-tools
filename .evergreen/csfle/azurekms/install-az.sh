2a3fc8475cd760a969dc608536d1601260b717e0
Kevin Albertson <kevin.albertson@mongodb.com>
2023-02-22 09:43:41 -0500

    DRIVERS-2411 skip check if az is installed

--- .evergreen/csfle/azurekms/install-az.sh
+++ .evergreen/csfle/azurekms/install-az.sh
@@ -6,10 +6,12 @@ set -o nounset
 # Install az CLI for Debian/Ubuntu.
 # https://learn.microsoft.com/en-us/cli/azure/install-azure-cli-linux?pivots=apt#option-2-step-by-step-installation-instructions
 echo "Install az ... begin"
-if command -v az &> /dev/null; then
-    echo "az is already installed"
-    exit 0
-fi
+# Always install the latest `az`. The debian11 distros may have an older version of `az` installed that do not support required options.
+# Once BUILD-16836 is resolved, uncomment the following to skip an unnecessary install attempt.
+# if command -v az &> /dev/null; then
+#     echo "az is already installed"
+#     exit 0
+# fi
 sudo apt-get update
 sudo apt-get install -y ca-certificates curl apt-transport-https lsb-release gnupg
 curl -sL https://packages.microsoft.com/keys/microsoft.asc |
