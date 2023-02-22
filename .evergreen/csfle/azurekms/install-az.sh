f49b0c5e91126b16f8c812f311aec385e9855ba0
Kevin Albertson <kevin.albertson@mongodb.com>
2023-02-22 14:41:16 -0500

    print version of `az`

--- .evergreen/csfle/azurekms/install-az.sh
+++ .evergreen/csfle/azurekms/install-az.sh
@@ -22,4 +22,5 @@ echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO 
     sudo tee /etc/apt/sources.list.d/azure-cli.list
 sudo apt-get update
 sudo apt-get install -y azure-cli
+az --version
 echo "Install az ... end"
