658953cb4633ecb9595535e4bd3946f407d8e0bc
Kevin Albertson <kevin.albertson@mongodb.com>
2023-02-23 14:37:11 -0500

    revise comment

--- .evergreen/csfle/azurekms/install-az.sh
+++ .evergreen/csfle/azurekms/install-az.sh
@@ -6,12 +6,7 @@ set -o nounset
 # Install az CLI for Debian/Ubuntu.
 # https://learn.microsoft.com/en-us/cli/azure/install-azure-cli-linux?pivots=apt#option-2-step-by-step-installation-instructions
 echo "Install az ... begin"
-# Always install the latest `az`. The debian11 distros may have an older version of `az` installed that do not support required options.
-# Once BUILD-16836 is resolved, uncomment the following to skip an unnecessary install attempt.
-# if command -v az &> /dev/null; then
-#     echo "az is already installed"
-#     exit 0
-# fi
+# Once a sufficient version of `az` is installed on all tested distros, remove the install-az.sh script. BUILD-16836 requests installation of the latest `az` on supported distros.
 sudo apt-get update
 sudo apt-get install -y ca-certificates curl apt-transport-https lsb-release gnupg
 curl -sL https://packages.microsoft.com/keys/microsoft.asc |
