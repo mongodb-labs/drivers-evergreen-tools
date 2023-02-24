ff4e2d652e25bf62c47e4367d6e21338f9e27061
Kevin Albertson <kevin.albertson@mongodb.com>
2023-02-24 08:29:27 -0500

    DRIVERS-2411 Do not skip install of `azure-cli` if `az` is detected (#271)
    * DRIVERS-2411 skip check if az is installed
    
    * print version of `az`

--- .evergreen/csfle/azurekms/install-az.sh
+++ .evergreen/csfle/azurekms/install-az.sh
@@ -6,10 +6,7 @@ set -o nounset
 # Install az CLI for Debian/Ubuntu.
 # https://learn.microsoft.com/en-us/cli/azure/install-azure-cli-linux?pivots=apt#option-2-step-by-step-installation-instructions
 echo "Install az ... begin"
-if command -v az &> /dev/null; then
-    echo "az is already installed"
-    exit 0
-fi
+# Once a sufficient version of `az` is installed on all tested distros, remove the install-az.sh script. BUILD-16836 requests installation of the latest `az` on supported distros.
 sudo apt-get update
 sudo apt-get install -y ca-certificates curl apt-transport-https lsb-release gnupg
 curl -sL https://packages.microsoft.com/keys/microsoft.asc |
@@ -20,4 +17,5 @@ echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO 
     sudo tee /etc/apt/sources.list.d/azure-cli.list
 sudo apt-get update
 sudo apt-get install -y azure-cli
+az --version
 echo "Install az ... end"
