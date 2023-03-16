6f416ce54bac16d812303b0bcf6f3cbd80dc38ef
Steven Silvester <steven.silvester@ieee.org>
2023-03-16 10:56:48 -0500

    DRIVERS-2415 Better wait for replicaset (#277)

--- .evergreen/auth_oidc/setup_oidc.js
+++ .evergreen/auth_oidc/setup_oidc.js
@@ -10,6 +10,10 @@ admin.runCommand({createRole: 'test1/readWrite', roles:[{role: 'readWrite', db: 
 admin.runCommand({createRole: 'test2/read', roles:[{role: 'read', db: 'test'}], privileges: []});
 
 // Wait for the node to be primary.
-while (admin.runCommand('replSetGetStatus').myState != 1) {}
+while (admin.runCommand('replSetGetStatus').myState != 1) {
+    sleep(50);
+}
+
+exit();
 
 }());
