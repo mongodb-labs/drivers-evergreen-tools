cbabc3b25b31ecd589decda0836224c0453c7b5f
Durran Jordan <durran@gmail.com>
2023-03-20 10:49:37 +0100

    test: remove sterr printout

--- .evergreen/run-deployed-lambda-aws-tests.sh
+++ .evergreen/run-deployed-lambda-aws-tests.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -o errexit  # Exit the script with error if any of the commands fail
-set -o xtrace   # Write all commands first to stderr
 
 # Explanation of required environment variables:
 #
