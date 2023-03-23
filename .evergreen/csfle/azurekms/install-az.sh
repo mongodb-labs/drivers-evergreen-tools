035d478de167a216999c1588ce908217a71986bb
Ezra Chung <ezra.chung@mongodb.com>
2023-03-23 15:07:41 -0500

    Add DPkg::Lock::Timeout to apt-get commands

--- .evergreen/csfle/azurekms/install-az.sh
+++ .evergreen/csfle/azurekms/install-az.sh
@@ -8,7 +8,7 @@ set -o nounset
 echo "Install az ... begin"
 # Once a sufficient version of `az` is installed on all tested distros, remove the install-az.sh script. BUILD-16836 requests installation of the latest `az` on supported distros.
 sudo apt-get update
-sudo apt-get install -y ca-certificates curl apt-transport-https lsb-release gnupg
+sudo apt-get -y -o DPkg::Lock::Timeout=-1 install ca-certificates curl apt-transport-https lsb-release gnupg
 curl -sL https://packages.microsoft.com/keys/microsoft.asc |
     gpg --dearmor |
     sudo tee /etc/apt/trusted.gpg.d/microsoft.gpg > /dev/null
@@ -16,6 +16,6 @@ AZ_REPO=$(lsb_release -cs)
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" |
     sudo tee /etc/apt/sources.list.d/azure-cli.list
 sudo apt-get update
-sudo apt-get install -y azure-cli
+sudo apt-get -y -o DPkg::Lock::Timeout=-1 install -y azure-cli
 az --version
 echo "Install az ... end"
--- .evergreen/csfle/gcpkms/remote-scripts/setup-gce-instance.sh
+++ .evergreen/csfle/gcpkms/remote-scripts/setup-gce-instance.sh
@@ -5,12 +5,12 @@ set -o errexit # Exit on first command error.
 echo "Installing dependencies ... begin"
 sudo apt-get update
 # Dependencies for mongod: https://www.mongodb.com/docs/manual/tutorial/install-mongodb-enterprise-on-debian-tarball/
-sudo apt-get -y install libcurl4 libgssapi-krb5-2 libldap-2.4-2 libwrap0 libsasl2-2 libsasl2-modules libsasl2-modules-gssapi-mit snmp openssl liblzma5
+sudo apt-get -y -o DPkg::Lock::Timeout=-1 install libcurl4 libgssapi-krb5-2 libldap-2.4-2 libwrap0 libsasl2-2 libsasl2-modules libsasl2-modules-gssapi-mit snmp openssl liblzma5
 # Dependencies for run-orchestration.sh
-sudo apt-get -y install virtualenv
-sudo apt-get -y install python3-pip
+sudo apt-get -y -o DPkg::Lock::Timeout=-1 install virtualenv
+sudo apt-get -y -o DPkg::Lock::Timeout=-1 install python3-pip
 # Install git.
-sudo apt-get -y install git
+sudo apt-get -y -o DPkg::Lock::Timeout=-1 install git
 echo "Installing dependencies ... end"
 
 echo "Starting MongoDB server ... begin"
