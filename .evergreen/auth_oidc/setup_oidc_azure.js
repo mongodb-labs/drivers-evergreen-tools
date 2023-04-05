5606f009be12f5b16bb652056271fd128d8a0bd5
Steven Silvester <steven.silvester@ieee.org>
2023-04-05 11:24:50 -0500

    debug output

--- .evergreen/auth_oidc/setup_oidc_azure.js
+++ .evergreen/auth_oidc/setup_oidc_azure.js
@@ -7,7 +7,9 @@
 const admin = Mongo().getDB("admin");
 assert(admin.auth("bob", "pwd123"));
 
+console.log("Setting up User");
 const authorizationClaim = process.env('AZUREOIDC_AUTH_CLAIM');
+console.log("authorizationClaim: ", authorizationClaim);
 
 // Add the role.
 admin.runCommand({createRole: 'OIDC_test/' + authorizationClaim, roles:[{role: 'readWrite', db: 'test'}], privileges: []});
