10bb2ef97baf38c2b9395db846a2006f80f14814
Steven Silvester <steven.silvester@ieee.org>
2023-04-12 08:00:15 -0500

    adding secrets handling

--- .evergreen/auth_oidc/azure/create-and-start-vm.sh
+++ .evergreen/auth_oidc/azure/create-and-start-vm.sh
@@ -19,29 +19,36 @@ if [ -z "${AZUREOIDC_VMNAME_PREFIX:-}" ] || \
     exit 1
 fi
 
-# TODO: Get resource group, public key path, private key path from vault
-
-# Translate env variables for KMS scripts.
+# Set defaults.
+BASE_PATH="$AZUREOIDC_DRIVERS_TOOLS/.evergreen/auth_oidc"
+export AZUREKMS_PUBLICKEYPATH="$BASE_PATH/azure/keyfile.pub"
+export AZUREKMS_PRIVATEKEYPATH="$BASE_PATH/azure/keyfile"
 export AZUREKMS_CLIENTID=$AZUREOIDC_CLIENTID
 export AZUREKMS_VMNAME_PREFIX=$AZUREOIDC_VMNAME_PREFIX
 export AZUREKMS_TENANTID=$AZUREOIDC_TENANTID
 export AZUREKMS_SECRET=$AZUREOIDC_SECRET
-export AZUREKMS_RESOURCEGROUP=$AZUREOIDC_RESOURCEGROUP
-export AZUREKMS_PUBLICKEYPATH=$AZUREOIDC_PUBLICKEYPATH
-export AZUREKMS_PRIVATEKEYPATH=$AZUREOIDC_PRIVATEKEYPATH
-
-# Set defaults.
+export AZUREOIDC_ENVPATH="$BASE_PATH/azure/env.sh"
 export AZUREOKMS_IMAGE=${AZUREOIDC_IMAGE:-"Debian:debian-11:11:0.20221020.1174"}
+
 # Install az.
-#"$AZUREOIDC_DRIVERS_TOOLS"/.evergreen/csfle/azurekms/install-az.sh
+"$AZUREOIDC_DRIVERS_TOOLS"/.evergreen/csfle/azurekms/install-az.sh
+
 # Login.
 "$AZUREOIDC_DRIVERS_TOOLS"/.evergreen/csfle/azurekms/login.sh
+
+# Handle secrets from vault.
+. ./activate-authoidcvenv.sh
+python $BASE_PATH/azure/handle_secrets.py
+source $AZUREOIDC_ENVPATH
+
 # Create VM.
 . "$AZUREOIDC_DRIVERS_TOOLS"/.evergreen/csfle/azurekms/create-vm.sh
 export AZUREOIDC_VMNAME="$AZUREOIDC_VMNAME"
 echo "AZUREOIDC_VMNAME: $AZUREOIDC_VMNAME" > testazurekms-expansions.yml
+
 # Assign role.
 "$AZUREOIDC_DRIVERS_TOOLS"/.evergreen/csfle/azurekms/assign-role.sh
+
 # Install dependencies.
 AZUREOIDC_SRC="$AZUREOIDC_DRIVERS_TOOLS/.evergreen/csfle/azurekms/remote-scripts/setup-azure-vm.sh" \
 AZUREOIDC_DST="./" \
@@ -50,17 +57,7 @@ AZUREOIDC_CMD="./setup-azure-vm.sh" \
     "$AZUREOIDC_DRIVERS_TOOLS"/.evergreen/csfle/azurekms/run-command.sh
 
 # Write the env variables file
-# TODO: these will be in the vault
-AZUREOIDC_SRC="$AZUREOIDC_DRIVERS_TOOLS/.evergreen/auth_oidc/azure/env.sh"
-cat <<EOF > $AZUREOIDC_SRC
-   export AZUREOIDC_CLIENTID=${AZUREOIDC_CLIENTID}
-   export AZUREOIDC_TENANTID=${AZUREOIDC_TENANTID}
-   export AZUREOIDC_APPID=${AZUREOIDC_APPID}
-   export AZUREOIDC_TOKENCLIENT=${AZUREOIDC_TOKENCLIENT}
-   export OIDC_AUTH_PREFIX=OIDC_test
-   export OIDC_AUTH_CLAIM=${AZUREOIDC_AUTH_CLAIM}
-EOF
-AZUREOIDC_SRC="$AZUREOIDC_SRC" \
+AZUREOIDC_SRC=$AZUREOIDC_ENVPATH \
 AZUREOIDC_DST="./" \
     "$AZUREOIDC_DRIVERS_TOOLS"/.evergreen/csfle/azurekms/copy-file.sh
 
@@ -70,7 +67,8 @@ AZUREOIDC_DST="./" \
     "$AZUREOIDC_DRIVERS_TOOLS"/.evergreen/csfle/azurekms/copy-file.sh
 AZUREOIDC_CMD="./start-mongodb.sh" \
     "$AZUREOIDC_DRIVERS_TOOLS"/.evergreen/csfle/azurekms/run-command.sh
-# Run the test
+
+# Run the self-test
 AZUREOIDC_SRC="$AZUREOIDC_DRIVERS_TOOLS/.evergreen/auth_oidc/azure/test.py" \
 AZUREOIDC_DST="./" \
     "$AZUREOIDC_DRIVERS_TOOLS"/.evergreen/csfle/azurekms/copy-file.sh
---
+++ .evergreen/auth_oidc/azure/handle_secrets.py
@@ -0,0 +1,45 @@
+from azure.keyvault.secrets import SecretClient
+from azure.identity import DefaultAzureCredential
+import os
+from base64 import b64decode
+
+def main():
+    vault_name = os.environ["AZUREOIDC_KEYVAULT"]
+    private_key_file = os.environ['AZUREKMS_PRIVATEKEYPATH']
+    public_key_file = os.environ['AZUREKMS_PUBLICKEYPATH']
+    env_file = os.environ['AZUREOIDC_ENVPATH']
+    client_id = os.environ['AZUREOIDC_CLIENTID']
+    tenant_id = os.environ['AZUREOIDC_TENANTID']
+    vault_uri = f"https://{vault_name}.vault.azure.net"
+
+    credential = DefaultAzureCredential()
+    client = SecretClient(vault_url=vault_uri, credential=credential)
+
+    secrets = dict()
+    for secret in ['RESOURCEGROUP', 'PUBLICKEY', 'PRIVATEKEY', 'TOKENCLIENT', 'AUTHCLAIM', 'AUTHPREFIX']:
+        retrieved = client.get_secret(secret)
+        secrets[secret] = retrieved.value
+
+    with open(env_file, 'w') as fid:
+        fid.write(f'export AZUREKMS_RESOURCEGROUP={secrets["RESOURCEGROUP"]}\n')
+        fid.write(f'export AZUREOIDC_TOKENCLIENT={secrets["TOKENCLIENT"]}\n')
+        fid.write(f'export AZUREOIDC_AUTHCLAIM={secrets["AUTHCLAIM"]}\n')
+        fid.write(f'export AZUREOIDC_CLIENTID={client_id}\n')
+        fid.write(f'export AZUREOIDC_TENANTID={tenant_id}\n')
+
+        # TODO: add to vault
+        fid.write(f'export AZUREOIDC_AUTHPREFIX={secrets["AUTHPREFIX"]}\n')
+
+    with open(private_key_file, 'w') as fid:
+        # TODO: this should be base64 in vault
+        fid.write(b64decode(secrets['PRIVATEKEY']).decode('utf8'))
+
+    with open(public_key_file, 'w') as fid:
+        # TODO: this should be base64 in vault
+        fid.write(b64decode(secrets['PUBLICKEY']).decode('utf8'))
+
+    return secrets
+
+
+if __name__ == '__main__':
+    main()
