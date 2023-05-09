2b89c453e9e735232fdde6f5a1fcf44c1361f9d1
Steven Silvester <steven.silvester@ieee.org>
2023-05-09 11:17:03 -0500

    try as a string

--- .evergreen/auth_aws/aws_tester.py
+++ .evergreen/auth_aws/aws_tester.py
@@ -50,7 +50,7 @@ def setup_assume_role():
     creds = json.loads(creds)
 
     # Create the user.
-    kwargs = dict(username=creds["AccessKeyId"], password=creds["SecretAccessKey"], authmechanismproperties=dict(AWS_SESSION_TOKEN=creds["SessionToken"]))
+    kwargs = dict(username=creds["AccessKeyId"], password=creds["SecretAccessKey"], authmechanismproperties=f"AWS_SESSION_TOKEN:creds['SessionToken']")
     create_user(ASSUMED_ROLE, kwargs)
 
 
@@ -119,7 +119,7 @@ def setup_web_identity():
     creds = json.loads(creds)
 
     # Create the user.
-    kwargs = dict(username=creds["AccessKeyId"], password=creds["SecretAccessKey"], authmechanismproperties=dict(AWS_SESSION_TOKEN=creds["SessionToken"]))
+    kwargs = dict(username=creds["AccessKeyId"], password=creds["SecretAccessKey"], authmechanismproperties=f"AWS_SESSION_TOKEN:creds['SessionToken']")
     create_user(ASSUMED_WEB_ROLE, kwargs)
 
 
