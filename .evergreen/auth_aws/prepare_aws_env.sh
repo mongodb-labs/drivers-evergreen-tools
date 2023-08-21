941aeba93cd0603cd320e92767934690750be0e0
Noah Stapp <noah.stapp@mongodb.com>
2023-08-21 15:49:52 -0700

    Update prepare_aws_env.sh

--- .evergreen/auth_aws/prepare_aws_env.sh
+++ .evergreen/auth_aws/prepare_aws_env.sh
@@ -6,15 +6,26 @@
 #   . ./prepare_aws_env.sh
 #
 # Loads AWS credentials for an authenticated MongoDB server. Exports the final credentials and server URI as environment variables.
+urlencode () {
+  $PYTHON_BINARY -c "import sys, urllib.parse as ulp; sys.stdout.write(ulp.quote_plus($1))"
+  }
 
-alias urlencode='${python3_binary} -c "import sys, urllib.parse as ulp; sys.stdout.write(ulp.quote_plus(sys.argv[1]))"'
-alias jsonkey='${python3_binary} -c "import json,sys;sys.stdout.write(json.load(sys.stdin)[sys.argv[1]])" < ${DRIVERS_TOOLS}/.evergreen/auth_aws/creds.json'
-USER=$(jsonkey AccessKeyId)
-USER=$(urlencode "$USER")
-PASS=$(jsonkey SecretAccessKey)
-PASS=$(urlencode "$PASS")
-SESSION_TOKEN=$(jsonkey SessionToken)
-SESSION_TOKEN=$(urlencode "$SESSION_TOKEN")
+if [ -n "$ASSUME_ROLE_CREDENTIALS" ]; then
+  jsonkey () {
+    $PYTHON_BINARY -c  "import json,sys;sys.stdout.write(json.load(sys.stdin)[$1])" < "${DRIVERS_TOOLS}"/.evergreen/auth_aws/creds.json
+    }
+
+  USER=$(jsonkey AccessKeyId)
+  USER=$(urlencode "$USER")
+  PASS=$(jsonkey SecretAccessKey)
+  PASS=$(urlencode "$PASS")
+  SESSION_TOKEN=$(jsonkey SessionToken)
+  SESSION_TOKEN=$(urlencode "$SESSION_TOKEN")
+else
+  USER=$(urlencode "${IAM_AUTH_ECS_ACCOUNT}")
+  PASS=$(urlencode "${IAM_AUTH_ECS_SECRET_ACCESS_KEY}")
+fi
+
 MONGODB_URI="mongodb://$USER:$PASS@localhost"
 
 export USER
