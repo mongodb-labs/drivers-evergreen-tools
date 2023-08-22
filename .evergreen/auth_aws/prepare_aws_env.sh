f06d7228fc0cd3b51f4da30c5efbe22818dff078
Noah Stapp <noah.stapp@mongodb.com>
2023-08-22 11:37:26 -0700

    Debugging more

--- .evergreen/auth_aws/prepare_aws_env.sh
+++ .evergreen/auth_aws/prepare_aws_env.sh
@@ -6,10 +6,11 @@
 #   . ./prepare_aws_env.sh
 #
 # Loads AWS credentials for an authenticated MongoDB server. Exports the final credentials and server URI as environment variables.
-$PYTHON_BINARY -c "import os;print('ENV VARS INSIDE HERE');print(sorted([(k, v[:2]) for k, v in os.environ.items()]))"
+
 urlencode () {
   $PYTHON_BINARY -c "import sys, urllib.parse as ulp; sys.stdout.write(ulp.quote_plus(sys.argv[1]))" "$1"
 }
+
 if [ -n "$ASSUME_ROLE_CREDENTIALS" ]; then
   jsonkey () {
     $PYTHON_BINARY -c  "import json,sys;sys.stdout.write(json.load(sys.stdin)[sys.argv[1]])" "$1" < "${DRIVERS_TOOLS}"/.evergreen/auth_aws/creds.json
@@ -21,14 +22,17 @@ if [ -n "$ASSUME_ROLE_CREDENTIALS" ]; then
   PASS=$(urlencode "$PASS")
   SESSION_TOKEN=$(jsonkey SessionToken)
   SESSION_TOKEN=$(urlencode "$SESSION_TOKEN")
+  export SESSION_TOKEN
 else
+  $PYTHON_BINARY -c "import os;print('ENV VARS INSIDE HERE');print(sorted([(k, v[:2]) for k, v in os.environ.items()]))"
   USER=$(urlencode "${IAM_AUTH_ECS_ACCOUNT}")
   PASS=$(urlencode "${IAM_AUTH_ECS_SECRET_ACCESS_KEY}")
 fi
 
 MONGODB_URI="mongodb://$USER:$PASS@localhost"
 
+echo "$MONGODB_URI"
+
 export USER
 export PASS
-export SESSION_TOKEN
 export MONGODB_URI
