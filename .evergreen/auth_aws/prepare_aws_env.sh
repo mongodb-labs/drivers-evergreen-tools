57f99e07a05241fafb3738ec58c6f2d0d75e38eb
Noah Stapp <noah.stapp@mongodb.com>
2023-08-22 14:35:42 -0700

    Don't echo prepare_aws_env

--- .evergreen/auth_aws/prepare_aws_env.sh
+++ .evergreen/auth_aws/prepare_aws_env.sh
@@ -6,6 +6,7 @@
 #   . ./prepare_aws_env.sh
 #
 # Loads AWS credentials for an authenticated MongoDB server. Exports the final credentials and server URI as environment variables.
+set -x
 
 urlencode () {
   $PYTHON_BINARY -c "import sys, urllib.parse as ulp; sys.stdout.write(ulp.quote_plus(sys.argv[1]))" "$1"
