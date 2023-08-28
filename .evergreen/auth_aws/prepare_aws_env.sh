c8c1dc86916cb2a8aa894660e554894f4d398e97
Noah Stapp <noah.stapp@mongodb.com>
2023-08-28 10:35:53 -0700

    Add  flag

--- .evergreen/auth_aws/prepare_aws_env.sh
+++ .evergreen/auth_aws/prepare_aws_env.sh
@@ -24,6 +24,18 @@ if [ -n "$ASSUME_ROLE_CREDENTIALS" ]; then
   SESSION_TOKEN=$(jsonkey SessionToken)
   SESSION_TOKEN=$(urlencode "$SESSION_TOKEN")
   export SESSION_TOKEN
+elif [ -n "$SESSION_TOKEN_CREDENTIALS" ]; then
+  jsonkey () {
+    $PYTHON_BINARY -c  "import json,sys;sys.stdout.write(json.load(sys.stdin)[sys.argv[1]])" "$1" < "${DRIVERS_TOOLS}"/.evergreen/auth_aws/creds.json
+  }
+
+  AWS_ACCESS_KEY_ID=$(jsonkey AccessKeyId)
+  AWS_SECRET_ACCESS_KEY=$(jsonkey SecretAccessKey)
+  AWS_SESSION_TOKEN=$(jsonkey SessionToken)
+
+  export AWS_ACCESS_KEY_ID
+  export AWS_SECRET_ACCESS_KEY
+  export AWS_SESSION_TOKEN
 else
   USER=$(urlencode "${IAM_AUTH_ECS_ACCOUNT}")
   PASS=$(urlencode "${IAM_AUTH_ECS_SECRET_ACCESS_KEY}")
@@ -31,6 +43,8 @@ fi
 
 MONGODB_URI="mongodb://$USER:$PASS@localhost"
 
-export USER
-export PASS
-export MONGODB_URI
+if [ -z "$SESSION_TOKEN_CREDENTIALS" ]; then
+  export USER
+  export PASS
+  export MONGODB_URI
+fi
