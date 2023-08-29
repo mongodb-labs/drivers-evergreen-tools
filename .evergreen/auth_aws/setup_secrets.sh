e20e541ab87c1548f018502215e547899b4ed8b7
Steven Silvester <steven.silvester@ieee.org>
2023-08-28 19:47:57 -0500

    GODRIVER-2958 Clean up secrets support

---
+++ .evergreen/auth_aws/setup_secrets.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# setup_secrets
+set -eu
+
+HERE=$(dirname $0)
+
+pushd $HERE
+. ./activate-authawsvenv.sh
+popd
+echo "Getting secrets: $@"
+python $HERE/setup_secrets.py $@
+echo "Got secrets"
