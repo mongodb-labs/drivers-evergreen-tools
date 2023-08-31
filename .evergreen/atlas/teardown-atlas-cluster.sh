ec378c8b4e233ef46fe413f6053346fb2b74c340
Bailey Pearson <bailey.pearson@mongodb.com>
2023-08-31 14:56:05 -0600

    ensure TASK_ID is present for teardown

--- .evergreen/atlas/teardown-atlas-cluster.sh
+++ .evergreen/atlas/teardown-atlas-cluster.sh
@@ -18,6 +18,7 @@ DRIVERS_ATLAS_PUBLIC_API_KEY
 DRIVERS_ATLAS_PRIVATE_API_KEY
 DRIVERS_ATLAS_GROUP_ID
 LAMBDA_STACK_NAME
+TASK_ID
 )
 
 # Ensure that all variables required to run the test are set, otherwise throw
