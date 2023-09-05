78464df925281da391c3203a96191306e90406af
Noah Stapp <noah.stapp@mongodb.com>
2023-09-05 10:21:28 -0700

    PYTHON 3920 - Migrate AWS Auth Tests to use AWS Secrets (#342)

--- .evergreen/auth_aws/aws_tester.py
+++ .evergreen/auth_aws/aws_tester.py
@@ -7,26 +7,33 @@ import os
 import json
 import sys
 import subprocess
+from functools import partial
+
 from pymongo import MongoClient
 from urllib.parse import quote_plus
-
 
 HERE = os.path.abspath(os.path.dirname(__file__))
 
 sys.path.insert(0, os.path.join(HERE, 'lib'))
+from util import get_key as _get_key
 from aws_assume_role import _assume_role
 from aws_assume_web_role import _assume_role_with_web_identity
 from aws_assign_instance_profile import _assign_instance_policy
 
-ASSUMED_ROLE = "arn:aws:sts::557821124784:assumed-role/authtest_user_assume_role/*";
+ASSUMED_ROLE = "arn:aws:sts::557821124784:assumed-role/authtest_user_assume_role/*"
 ASSUMED_WEB_ROLE = "arn:aws:sts::857654397073:assumed-role/webIdentityTestRole/*"
 
 # This varies based on hosting EC2 as the account id and role name can vary
 AWS_ACCOUNT_ARN = "arn:aws:sts::557821124784:assumed-role/evergreen_task_hosts_instance_role_production/*"
+_USE_AWS_SECRETS = False
 
-
-with open(os.path.join(HERE, 'aws_e2e_setup.json')) as fid:
-   CONFIG = json.load(fid)
+try:
+    with open(os.path.join(HERE, 'aws_e2e_setup.json')) as fid:
+        CONFIG = json.load(fid)
+        get_key = partial(_get_key, uppercase=False)
+except FileNotFoundError:
+    CONFIG = os.environ
+    get_key = partial(_get_key, uppercase=True)
 
 
 def run(args, env):
@@ -50,25 +57,26 @@ def create_user(user, kwargs):
 
 def setup_assume_role():
     # Assume the role to get temp creds.
-    os.environ['AWS_ACCESS_KEY_ID'] = CONFIG["iam_auth_assume_aws_account"]
-    os.environ['AWS_SECRET_ACCESS_KEY'] = CONFIG["iam_auth_assume_aws_secret_access_key"]
+    os.environ['AWS_ACCESS_KEY_ID'] = CONFIG[get_key("iam_auth_assume_aws_account")]
+    os.environ['AWS_SECRET_ACCESS_KEY'] = CONFIG[get_key("iam_auth_assume_aws_secret_access_key")]
 
-    role_name = CONFIG["iam_auth_assume_role_name"]
+    role_name = CONFIG[get_key("iam_auth_assume_role_name")]
     creds = _assume_role(role_name)
     with open(os.path.join(HERE, 'creds.json'), 'w') as fid:
         json.dump(creds, fid)
 
     # Create the user.
     token = quote_plus(creds['SessionToken'])
-    kwargs = dict(username=creds["AccessKeyId"], password=creds["SecretAccessKey"], authmechanismproperties=f"AWS_SESSION_TOKEN:{token}")
+    kwargs = dict(username=creds["AccessKeyId"], password=creds["SecretAccessKey"],
+                  authmechanismproperties=f"AWS_SESSION_TOKEN:{token}")
     create_user(ASSUMED_ROLE, kwargs)
 
 
 def setup_ec2():
     # Create the user.
     _assign_instance_policy()
-    del os.environ['AWS_ACCESS_KEY_ID']
-    del os.environ['AWS_SECRET_ACCESS_KEY']
+    os.environ.pop("AWS_ACCESS_KEY_ID", None)
+    os.environ.pop("AWS_SECRET_ACCESS_KEY", None)
     create_user(AWS_ACCOUNT_ARN, dict())
 
 
@@ -77,14 +85,14 @@ def setup_ecs():
     mongo_binaries = os.environ['MONGODB_BINARIES']
     project_dir = os.environ['PROJECT_DIRECTORY']
     base_command = f"{sys.executable} -u  lib/container_tester.py"
-    run_prune_command = f"{base_command} -v remote_gc_services --cluster {CONFIG['iam_auth_ecs_cluster']}"
-    run_test_command = f"{base_command} -d -v run_e2e_test --cluster {CONFIG['iam_auth_ecs_cluster']} --task_definition {CONFIG['iam_auth_ecs_task_definition']} --subnets {CONFIG['iam_auth_ecs_subnet_a']} --subnets {CONFIG['iam_auth_ecs_subnet_b']} --security_group {CONFIG['iam_auth_ecs_security_group']} --files {mongo_binaries}/mongod:/root/mongod {mongo_binaries}/mongosh:/root/mongosh lib/ecs_hosted_test.js:/root/ecs_hosted_test.js {project_dir}:/root --script lib/ecs_hosted_test.sh"
+    run_prune_command = f"{base_command} -v remote_gc_services --cluster {CONFIG[get_key('iam_auth_ecs_cluster')]}"
+    run_test_command = f"{base_command} -d -v run_e2e_test --cluster {CONFIG[get_key('iam_auth_ecs_cluster')]} --task_definition {CONFIG[get_key('iam_auth_ecs_task_definition')]} --subnets {CONFIG[get_key('iam_auth_ecs_subnet_a')]} --subnets {CONFIG[get_key('iam_auth_ecs_subnet_b')]} --security_group {CONFIG[get_key('iam_auth_ecs_security_group')]} --files {mongo_binaries}/mongod:/root/mongod {mongo_binaries}/mongosh:/root/mongosh lib/ecs_hosted_test.js:/root/ecs_hosted_test.js {project_dir}:/root --script lib/ecs_hosted_test.sh"
 
     # Pass in the AWS credentials as environment variables
     # AWS_SHARED_CREDENTIALS_FILE does not work in evergreen for an unknown
     #  reason
-    env = dict(AWS_ACCESS_KEY_ID=CONFIG['iam_auth_ecs_account'],
-               AWS_SECRET_ACCESS_KEY=CONFIG['iam_auth_ecs_secret_access_key'])
+    env = dict(AWS_ACCESS_KEY_ID=CONFIG[get_key('iam_auth_ecs_account')],
+               AWS_SECRET_ACCESS_KEY=CONFIG[get_key('iam_auth_ecs_secret_access_key')])
 
     # Prune other containers
     subprocess.check_call(['/bin/sh', '-c', run_prune_command], env=env)
@@ -92,22 +100,23 @@ def setup_ecs():
     # Run the test in a container
     subprocess.check_call(['/bin/sh', '-c', run_test_command], env=env)
 
+
 def setup_regular():
     # Create the user.
     kwargs = dict(
-        username=CONFIG["iam_auth_ecs_account"],
-        password=CONFIG["iam_auth_ecs_secret_access_key"]
+        username=CONFIG[get_key("iam_auth_ecs_account")],
+        password=CONFIG[get_key("iam_auth_ecs_secret_access_key")]
     )
-    create_user(CONFIG["iam_auth_ecs_account_arn"], kwargs)
+    create_user(CONFIG[get_key("iam_auth_ecs_account_arn")], kwargs)
 
 
 def setup_web_identity():
     # Unassign the instance profile.
-    env = dict(AWS_ACCESS_KEY_ID=CONFIG["iam_auth_ec2_instance_account"],
-               AWS_SECRET_ACCESS_KEY=CONFIG["iam_auth_ec2_instance_secret_access_key"])
+    env = dict(AWS_ACCESS_KEY_ID=CONFIG[get_key("iam_auth_ec2_instance_account")],
+               AWS_SECRET_ACCESS_KEY=CONFIG[get_key("iam_auth_ec2_instance_secret_access_key")])
     ret = run(['lib/aws_unassign_instance_profile.py'], env)
     if ret == 2:
-        raise RuntimeError("Request limit exceeded for AWS API");
+        raise RuntimeError("Request limit exceeded for AWS API")
 
     if ret != 0:
         print('ret was', ret)
@@ -115,10 +124,10 @@ def setup_web_identity():
 
     # Handle the OIDC credentials.
     env = dict(
-        IDP_ISSUER=CONFIG["iam_web_identity_issuer"],
-        IDP_JWKS_URI=CONFIG["iam_web_identity_jwks_uri"],
-        IDP_RSA_KEY=CONFIG["iam_web_identity_rsa_key"],
-        AWS_WEB_IDENTITY_TOKEN_FILE=CONFIG['iam_web_identity_token_file']
+        IDP_ISSUER=CONFIG[get_key("iam_web_identity_issuer")],
+        IDP_JWKS_URI=CONFIG[get_key("iam_web_identity_jwks_uri")],
+        IDP_RSA_KEY=CONFIG[get_key("iam_web_identity_rsa_key")],
+        AWS_WEB_IDENTITY_TOKEN_FILE=CONFIG[get_key('iam_web_identity_token_file')]
     )
 
     ret = run(['lib/aws_handle_oidc_creds.py', 'token'], env)
@@ -126,8 +135,8 @@ def setup_web_identity():
         raise RuntimeWarning("Failed to write the web token")
 
     # Assume the web role to get temp credentials.
-    os.environ['AWS_WEB_IDENTITY_TOKEN_FILE'] = CONFIG['iam_web_identity_token_file']
-    os.environ['AWS_ROLE_ARN'] = CONFIG["iam_auth_assume_web_role_name"]
+    os.environ['AWS_WEB_IDENTITY_TOKEN_FILE'] = CONFIG[get_key('iam_web_identity_token_file')]
+    os.environ['AWS_ROLE_ARN'] = CONFIG[get_key("iam_auth_assume_web_role_name")]
 
     creds = _assume_role_with_web_identity()
     with open(os.path.join(HERE, 'creds.json'), 'w') as fid:
@@ -135,7 +144,8 @@ def setup_web_identity():
 
     # Create the user.
     token = quote_plus(creds['SessionToken'])
-    kwargs = dict(username=creds["AccessKeyId"], password=creds["SecretAccessKey"], authmechanismproperties=f"AWS_SESSION_TOKEN:{token}")
+    kwargs = dict(username=creds["AccessKeyId"], password=creds["SecretAccessKey"],
+                  authmechanismproperties=f"AWS_SESSION_TOKEN:{token}")
     create_user(ASSUMED_WEB_ROLE, kwargs)
 
 
--- .evergreen/auth_aws/lib/aws_assign_instance_profile.py
+++ .evergreen/auth_aws/lib/aws_assign_instance_profile.py
@@ -10,9 +10,14 @@ import json
 import os
 import sys
 import time
+from functools import partial
 
 import boto3
 import botocore
+
+from util import get_key as _get_key
+
+sys.path.insert(1, os.path.join(sys.path[0], '..'))
 
 LOGGER = logging.getLogger(__name__)
 HERE = os.path.abspath(os.path.dirname(__file__))
@@ -20,6 +25,7 @@ HERE = os.path.abspath(os.path.dirname(__file__))
 
 def _get_local_instance_id():
     return urllib.request.urlopen('http://169.254.169.254/latest/meta-data/instance-id').read().decode()
+
 
 def _has_instance_profile():
     base_url = "http://169.254.169.254/latest/meta-data/iam/security-credentials/"
@@ -45,6 +51,7 @@ def _has_instance_profile():
 
     return True
 
+
 def _wait_instance_profile():
     retry = 60
     while not _has_instance_profile() and retry:
@@ -54,14 +61,22 @@ def _wait_instance_profile():
     if retry == 0:
         raise ValueError("Timeout on waiting for instance profile")
 
+
 def _handle_config():
     try:
         with open(os.path.join(HERE, '..', 'aws_e2e_setup.json')) as fid:
             CONFIG = json.load(fid)
+            get_key = partial(_get_key, uppercase=False)
 
-        os.environ.setdefault('AWS_ACCESS_KEY_ID', CONFIG['iam_auth_ec2_instance_account'])
-        os.environ.setdefault('AWS_SECRET_ACCESS_KEY', CONFIG['iam_auth_ec2_instance_secret_access_key'])
-        return CONFIG['iam_auth_ec2_instance_profile']
+    except FileNotFoundError:
+        CONFIG = os.environ
+        get_key = partial(_get_key, uppercase=True)
+
+    try:
+        os.environ.setdefault('AWS_ACCESS_KEY_ID', CONFIG[get_key('iam_auth_ec2_instance_account')])
+        os.environ.setdefault('AWS_SECRET_ACCESS_KEY',
+                              CONFIG[get_key('iam_auth_ec2_instance_secret_access_key')])
+        return CONFIG[get_key('iam_auth_ec2_instance_profile')]
     except Exception as e:
         print(e)
         return ''
@@ -69,8 +84,8 @@ def _handle_config():
 
 DEFAULT_ARN = _handle_config()
 
-def _assign_instance_policy(iam_instance_arn=DEFAULT_ARN):
 
+def _assign_instance_policy(iam_instance_arn=DEFAULT_ARN):
     if _has_instance_profile():
         print("IMPORTANT: Found machine already has instance profile, skipping the assignment")
         return
@@ -79,13 +94,13 @@ def _assign_instance_policy(iam_instance_arn=DEFAULT_ARN):
 
     ec2_client = boto3.client("ec2", 'us-east-1')
 
-    #https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ec2.html#EC2.Client.associate_iam_instance_profile
+    # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ec2.html#EC2.Client.associate_iam_instance_profile
     try:
         response = ec2_client.associate_iam_instance_profile(
             IamInstanceProfile={
-                'Arn' : iam_instance_arn,
+                'Arn': iam_instance_arn,
             },
-            InstanceId = instance_id)
+            InstanceId=instance_id)
 
         print(response)
 
@@ -110,7 +125,6 @@ def main() -> None:
     parser.add_argument('--instance_profile_arn', type=str, help="Name of instance profile", default=DEFAULT_ARN)
 
     args = parser.parse_args()
-
 
     if args.debug:
         logging.basicConfig(level=logging.DEBUG)
