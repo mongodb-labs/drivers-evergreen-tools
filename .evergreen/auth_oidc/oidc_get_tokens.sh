8ce38dea50ea726a734037c28bf8983907bb1129
Steven Silvester <steven.silvester@ieee.org>
2023-09-05 20:18:07 -0500

    fix handling of temp dir

--- .evergreen/auth_oidc/oidc_get_tokens.sh
+++ .evergreen/auth_oidc/oidc_get_tokens.sh
@@ -3,10 +3,14 @@
 # Get the set of OIDC tokens in the OIDC_TOKEN_DIR.
 #
 set -eux
-export OIDC_TOKEN_DIR=${OIDC_TOKEN_DIR:-/tmp/tokens}
+if [ -z "$OIDC_TOKEN_DIR" ]; then
+    if [ "Windows_NT" = "$OS" ];
+        export OIDC_TOKEN_DIR=C:/Temp/tokens
+    else
+        export OIDC_TOKEN_DIR=/tmp/tokens
+    fi
+fi
 mkdir -p $OIDC_TOKEN_DIR
 . ./activate-authoidcvenv.sh
 python oidc_get_tokens.py
-echo "before"
 ls -ltr $OIDC_TOKEN_DIR
-echo "after"
