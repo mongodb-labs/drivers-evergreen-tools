7f7120257637e16ef4a82b315a07147258609761
Steven Silvester <steven.silvester@ieee.org>
2023-09-23 18:22:54 -0500

    DRIVERS-2543 Pull mongohouse image from ADL ECR repo

--- .evergreen/atlas_data_lake/build-mongohouse-local.sh
+++ .evergreen/atlas_data_lake/build-mongohouse-local.sh
@@ -1,69 +1,11 @@
 #!/bin/sh
 #
-# This script builds a local mongohoused for testing.
+# This script sets up the local docker image for mongohoused.
+#
+set -eu
 
-set -o errexit  # Exit the script with error if any of the commands fail
-
-ORIG_DIR="$(pwd)"
-
-# Configure git to use the git protocol
-git config --global url."git@github.com:".insteadof "https://github.com/"
-
-AP_START=$(date +%s)
-
-# Set up environment variables for Go
-GO_VERSION=1.20
-if [ "Windows_NT" = "$OS" ]; then
-  export GOPATH="$(cygpath -m "$(pwd)")/.gopath"
-  export GOCACHE="$(cygpath -m "$(pwd)")/.cache"
-  export GOROOT="C:/golang/go${GO_VERSION}"
-  export PATH="/cygdrive/c/golang/go${GO_VERSION}/bin:$PATH"
-else
-  export GOPATH=`pwd`/.gopath
-  export GOROOT="/opt/golang/go${GO_VERSION}"
-  export PATH="$GOROOT/bin:$PATH"
-fi;
-go version
-
-# Clone the mongohouse repo
-DL_START=$(date +%s)
-cd "$ORIG_DIR"
-rm -rf mongohouse
-git clone git@github.com:10gen/mongohouse.git
-cd mongohouse
-GO111MODULE=on go mod download
-DL_END=$(date +%s)
-
-# Build mqlrun
-./build.sh tools:download:mqlrun
-export MONGOHOUSE_MQLRUN=`pwd`/artifacts/mqlrun
-
-# Build mongohouse
-./build.sh build:mongohoused
-
-sleep 5
-
-AP_END=$(date +%s)
-
-# Write results file
-DL_ELAPSED=$(expr $DL_END - $DL_START)
-AP_ELAPSED=$(expr $AP_END - $AP_START)
-cat <<EOT >> $DRIVERS_TOOLS/results.json
-{"results": [
-  {
-    "status": "PASS",
-    "test_file": "Mongohouse local Start",
-    "start": $AP_START,
-    "end": $AP_END,
-    "elapsed": $AP_ELAPSED
-  },
-  {
-    "status": "PASS",
-    "test_file": "Download Mongohouse",
-    "start": $DL_START,
-    "end": $DL_END,
-    "elapsed": $DL_ELAPSED
-  }
-]}
-
-EOT
+DRIVERS_TOOLS=${DRIVERS_TOOLS:-$(readlink -f ../..)}
+bash $DRIVERS_TOOLS/.evergreen/auth_aws/setup_secrets.sh drivers/adl
+source secrets-export.sh
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 904697982180.dkr.ecr.us-east-1.amazonaws.com/atlas-query-engine-test
+docker pull 904697982180.dkr.ecr.us-east-1.amazonaws.com/atlas-query-engine-test
--- .evergreen/atlas_data_lake/run-mongohouse-local.sh
+++ .evergreen/atlas_data_lake/run-mongohouse-local.sh
@@ -1,14 +1,10 @@
 #!/bin/sh
 #
-# This script launches a pre-built local mongohoused for testing.
+# This script launches a docker-based for mongohoused for testing.
 #
 # There is no corresponding 'shutdown' script; this project relies
 # on Evergreen to terminate processes and clean up when tasks end.
+set -eu
 
-cd mongohouse
-if [ "Windows_NT" = "$OS" ]; then
-  export MONGOHOUSE_MQLRUN="$(cygpath -m "$(pwd)")/artifacts/mqlrun.exe"
-else
-  export MONGOHOUSE_MQLRUN=`pwd`/artifacts/mqlrun
-fi;
-./artifacts/mongohoused --config ${DRIVERS_TOOLS}/.evergreen/atlas_data_lake/config.yml
+DRIVERS_TOOLS=${DRIVERS_TOOLS:-$(readlink -f ../..)}
+docker run -e DRIVERS_TOOLS=$DRIVERS_TOOLS -it 904697982180.dkr.ecr.us-east-1.amazonaws.com/atlas-query-engine-test --config ./testdata/config/external/drivers/config.yaml
