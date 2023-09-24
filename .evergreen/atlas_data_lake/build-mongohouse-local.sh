1b8e7c657feb6c4ae80af4250436fd5fdb501a4a
Steven Silvester <steven.silvester@ieee.org>
2023-09-23 20:58:52 -0500

    fix handling of token

--- .evergreen/atlas_data_lake/build-mongohouse-local.sh
+++ .evergreen/atlas_data_lake/build-mongohouse-local.sh
@@ -8,6 +8,7 @@ DRIVERS_TOOLS=${DRIVERS_TOOLS:-$(readlink -f ../..)}
 pushd $DRIVERS_TOOLS/.evergreen/auth_aws
 bash setup_secrets.sh drivers/adl
 source secrets-export.sh
+unset AWS_SESSION_TOKEN
 popd
 DOCKER=$(command -v docker || command -v podman)
 aws ecr get-login-password --region us-east-1 | $DOCKER login --username AWS --password-stdin 904697982180.dkr.ecr.us-east-1.amazonaws.com/atlas-query-engine-test
