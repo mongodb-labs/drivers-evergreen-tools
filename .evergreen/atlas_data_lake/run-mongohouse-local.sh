4ffed8942c880f18d65efb33b36a31631dfb027b
Steven Silvester <steven.silvester@ieee.org>
2023-09-24 09:15:50 -0500

    add sleep and debug print

--- .evergreen/atlas_data_lake/run-mongohouse-local.sh
+++ .evergreen/atlas_data_lake/run-mongohouse-local.sh
@@ -4,10 +4,11 @@
 #
 # There is no corresponding 'shutdown' script; this project relies
 # on Evergreen to terminate processes and clean up when tasks end.
-set -eu
+set -eux
 
 DRIVERS_TOOLS=${DRIVERS_TOOLS:-$(readlink -f ../..)}
 DOCKER=$(command -v docker || command -v podman)
 USE_TTY=""
 test -t 1 && USE_TTY="-t"
 $DOCKER run -e DRIVERS_TOOLS=$DRIVERS_TOOLS -p 27017:27017 -i $USE_TTY 904697982180.dkr.ecr.us-east-1.amazonaws.com/atlas-query-engine-test --config ./testdata/config/external/drivers/config.yaml
+sleep 10
