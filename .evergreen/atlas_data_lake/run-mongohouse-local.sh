f2a381a85466e25837dc0c7f246d8548eb3097a9
Steven Silvester <steven.silvester@ieee.org>
2023-09-24 21:16:11 -0500

    remove sleep

--- .evergreen/atlas_data_lake/run-mongohouse-local.sh
+++ .evergreen/atlas_data_lake/run-mongohouse-local.sh
@@ -11,4 +11,3 @@ DOCKER=$(command -v docker || command -v podman)
 USE_TTY=""
 test -t 1 && USE_TTY="-t"
 $DOCKER run -e DRIVERS_TOOLS=$DRIVERS_TOOLS -p 27017:27017 -i $USE_TTY 904697982180.dkr.ecr.us-east-1.amazonaws.com/atlas-query-engine-test --config ./testdata/config/external/drivers/config.yaml
-sleep 10
