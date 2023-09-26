c31bb8a48777cd1bc77cd7240b62112e3de30747
Steven Silvester <steven.silvester@ieee.org>
2023-09-26 10:40:33 -0500

    address review

--- .evergreen/docker/ubuntu20.04/base-entrypoint.sh
+++ .evergreen/docker/ubuntu20.04/base-entrypoint.sh
@@ -5,7 +5,7 @@ rm -f $DRIVERS_TOOLS/results.json
 cd $DRIVERS_TOOLS/.evergreen
 bash run-orchestration.sh
 
-# Change permissions of files we have created.
+# Preserve host permissions of files we have created.
 cd $DRIVERS_TOOLS
 files=(results.json uri.txt .evergreen/mongo_crypt_v1.so .evergreen/mo-expansion.yml)
 chown --reference=action.yml "$files[@]"
