ef56e8b175388a4debc755b11decc11225452f33
Steven Silvester <steven.silvester@ieee.org>
2023-10-12 09:43:56 -0500

    fix path handling

--- .evergreen/github_app/create_or_modify_comment.sh
+++ .evergreen/github_app/create_or_modify_comment.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -o errexit  # Exit the script with error if any of the commands fail
 
-bash $DRIVERS_TOOLS/.evergreen/auth_aws/setup_secrets.sh drivers/comment-bot
 DIR=$(dirname $0)
 pushd $DIR
 
@@ -14,6 +13,7 @@ nvm install lts/hydrogen
 nvm use lts/hydrogen
 
 npm install
+bash $DRIVERS_TOOLS/.evergreen/auth_aws/setup_secrets.sh drivers/comment-bot
 source secrets-export.sh
 node create_or_modify_comment.js "$@"
 popd
