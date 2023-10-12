ee79904ebc91d813119f74f6fca2a32fc3b49d38
Steven Silvester <steven.silvester@ieee.org>
2023-10-12 09:34:17 -0500

    bootstrap node

--- .evergreen/github_app/create_or_modify_comment.sh
+++ .evergreen/github_app/create_or_modify_comment.sh
@@ -4,6 +4,15 @@ set -o errexit  # Exit the script with error if any of the commands fail
 bash $DRIVERS_TOOLS/.evergreen/auth_aws/setup_secrets.sh drivers/comment-bot
 DIR=$(dirname $0)
 pushd $DIR
+
+# Bootstrap the appropriate Hydrogen LTS version of node.
+# https://github.com/nvm-sh/nvm?tab=readme-ov-file#installing-and-updating
+export NVM_DIR=$(pwd)
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+nvm install lts/hydrogen
+nvm use lts/hydrogen
+
 npm install
 source secrets-export.sh
 node create_or_modify_comment.js "$@"
