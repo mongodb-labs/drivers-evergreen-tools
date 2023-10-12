4ffa5020a030c9ffc1b6558373573833a13205f0
Steven Silvester <steven.silvester@ieee.org>
2023-10-12 07:14:30 -0500

    handle dir

--- .evergreen/github_app/create_or_modify_comment.sh
+++ .evergreen/github_app/create_or_modify_comment.sh
@@ -2,6 +2,9 @@
 set -o errexit  # Exit the script with error if any of the commands fail
 
 bash $DRIVERS_TOOLS/.evergreen/auth_aws/setup_secrets.sh drivers/comment-bot
+DIR=$(dirname $0)
+pushd $DIR
 npm install
 source secrets-export.sh
 node create_or_modify_comment.js "$@"
+popd
