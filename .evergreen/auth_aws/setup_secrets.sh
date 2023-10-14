0f0018dcd5314f6da756c77c5c2e64cbfb639c07
Steven Silvester <steven.silvester@ieee.org>
2023-10-14 11:33:30 -0500

    debug

--- .evergreen/auth_aws/setup_secrets.sh
+++ .evergreen/auth_aws/setup_secrets.sh
@@ -6,6 +6,8 @@ HERE=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 pushd $HERE
 . ./activate-authawsvenv.sh
 popd
+set -x
 echo "Getting secrets:" "$@"
+which python
 python $HERE/setup_secrets.py "$@"
 echo "Got secrets"
