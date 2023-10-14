1cb03685dd84eb26d4556611c355d8b842e2dbb2
Steven Silvester <steven.silvester@ieee.org>
2023-10-14 07:48:22 -0500

    try again

--- .evergreen/auth_aws/setup_secrets.sh
+++ .evergreen/auth_aws/setup_secrets.sh
@@ -3,14 +3,12 @@
 set -eu
 
 HERE=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-
+if [ "Windows_NT" = "$OS:-" ]; then
+    HERE=$(cygpath $HERE)
+fi
 pushd $HERE
 . ./activate-authawsvenv.sh
 popd
 echo "Getting secrets:" "$@"
-target=$HERE/setup_secrets.py
-if [ "Windows_NT" = "$OS" ]; then
-    target=$(cygpath $target)
-fi
-python $target "$@"
+python $HERE/setup_secrets.py "$@"
 echo "Got secrets"
