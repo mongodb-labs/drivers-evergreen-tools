50d668e1bb78db880fff8649b534f2185da87e7f
Steven Silvester <steven.silvester@ieee.org>
2023-11-05 20:35:50 -0600

    use updated config

--- .evergreen/atlas_data_lake/run-mongohouse-image.sh
+++ .evergreen/atlas_data_lake/run-mongohouse-image.sh
@@ -10,4 +10,4 @@ DRIVERS_TOOLS=${DRIVERS_TOOLS:-$(readlink -f ../..)}
 IMAGE=904697982180.dkr.ecr.us-east-1.amazonaws.com/atlas-query-engine-test
 USE_TTY=""
 test -t 1 && USE_TTY="-t"
-docker run -d -p 27017:27017 -v "$DRIVERS_TOOLS/.evergreen/atlas_data_lake:/src" -i $USE_TTY $IMAGE --config /src/test.yaml
+docker run -d -p 27017:27017 -v "$DRIVERS_TOOLS/.evergreen/atlas_data_lake:/src" -i $USE_TTY $IMAGE --config ./testdata/config/external/drivers/config.yaml
