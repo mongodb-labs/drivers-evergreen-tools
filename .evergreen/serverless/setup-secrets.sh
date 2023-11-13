089981f35e2a7b29b767312329b8c586fe7889e9
Steven Silvester <steven.silvester@ieee.org>
2023-11-13 11:57:11 -0600

    add setup file

---
+++ .evergreen/serverless/setup-secrets.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+#
+# setup-secrets.sh
+#
+# Set up serverless secrets using Drivers AWS Secrets Manager.
+# Writes a secrets-export.sh file to this folder.
+set -eux
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+pushd $SCRIPT_DIR
+bash ../aws_auth/setup_secrets.sh drivers/serverless
