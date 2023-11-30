507fa37f10de59e736205b2afc3cfe132cae6717
Steven Silvester <steven.silvester@ieee.org>
2023-11-30 14:58:43 -0600

    try this

--- .evergreen/csfle/azurekms/remote-scripts/setup-azure-vm.sh
+++ .evergreen/csfle/azurekms/remote-scripts/setup-azure-vm.sh
@@ -2,19 +2,18 @@
 set -o errexit
 set -o pipefail
 # Do not error on unset variables. run-orchestration.sh accesses unset variables.
-export DEBIAN_FRONTEND=noninteractive
-sudo apt-get update
+DEBIAN_FRONTEND=noninteractive sudo apt-get update
 
 echo "Install jq ... begin"
-sudo apt-get -y -o DPkg::Lock::Timeout=-1 install jq
+DEBIAN_FRONTEND=noninteractive sudo apt-get -y -o DPkg::Lock::Timeout=-1 install jq
 echo "Install jq ... end"
 
 echo "Installing MongoDB dependencies ... begin"
 # Dependencies for mongod: https://www.mongodb.com/docs/manual/tutorial/install-mongodb-enterprise-on-debian-tarball/
-sudo apt-get -y -o DPkg::Lock::Timeout=-1 install libcurl4 libgssapi-krb5-2 libldap-2.4-2 libwrap0 libsasl2-2 libsasl2-modules libsasl2-modules-gssapi-mit snmp openssl liblzma5
+DEBIAN_FRONTEND=noninteractive sudo apt-get -y -o DPkg::Lock::Timeout=-1 install libcurl4 libgssapi-krb5-2 libldap-2.4-2 libwrap0 libsasl2-2 libsasl2-modules libsasl2-modules-gssapi-mit snmp openssl liblzma5
 # Dependencies for run-orchestration.sh
-sudo apt-get -y -o DPkg::Lock::Timeout=-1 install python3.9-venv
-sudo apt-get -y -o DPkg::Lock::Timeout=-1 install python3-pip
+DEBIAN_FRONTEND=noninteractive sudo apt-get -y -o DPkg::Lock::Timeout=-1 install python3.9-venv
+DEBIAN_FRONTEND=noninteractive sudo apt-get -y -o DPkg::Lock::Timeout=-1 install python3-pip
 # Install git.
-sudo apt-get -y -o DPkg::Lock::Timeout=-1 install git
+DEBIAN_FRONTEND=noninteractive sudo apt-get -y -o DPkg::Lock::Timeout=-1 install git
 echo "Installing MongoDB dependencies ... end"
