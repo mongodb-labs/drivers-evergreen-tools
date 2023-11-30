a1f344837f63a895dd80eaddc7aa8d4f6ec4772f
Steven Silvester <steven.silvester@ieee.org>
2023-11-30 10:17:39 -0600

    fix handling of run test

--- .evergreen/auth_oidc/azure/run-driver-test.sh
+++ .evergreen/auth_oidc/azure/run-driver-test.sh
@@ -5,22 +5,25 @@ set -o nounset
 
 # Check for inputs.
 if [ -z "${AZUREOIDC_DRIVERS_TAR_FILE:-}" ] || \
-   [ -z "${AZUREOIDC_TEST_CMD:-}" ]; then
+   [ -z "${AZUREOIDC_TEST_CMD:-}" ] || \
+   [ -z "${AZUREOIDC_DRIVERS_TOOLS:-}" ] then;
     echo "Please set the following required environment variables"
     echo " AZUREOIDC_DRIVERS_TAR_FILE"
     echo " AZUREOIDC_TEST_CMD"
+    echo " AZUREOIDC_DRIVERS_TOOLS"
     exit 1
 fi
 
 # Read in the env variables.
-DIR=$(dirname $0)
-source $DIR/env.sh
+DRIVERS_TOOLS=$AZUREOIDC_DRIVERS_TOOLS
+AZURE_DIR=$DRIVERS_TOOLS/.evergreen/auth_oidc/azure
+source $AZURE_DIR/env.sh
 
 # Set up variables.
 DRIVERS_TOOLS=$AZUREOIDC_DRIVERS_TOOLS
 export AZUREKMS_RESOURCEGROUP=$AZUREOIDC_RESOURCEGROUP
 export AZUREKMS_VMNAME=$AZUREOIDC_VMNAME
-export AZUREKMS_PRIVATEKEYPATH=$DIR/keyfile
+export AZUREKMS_PRIVATEKEYPATH=$AZURE_DIR/keyfile
 
 # Set up the remote driver checkout.
 DRIVER_TARFILE_BASE=$(basename ${AZUREOIDC_DRIVERS_TAR_FILE})
