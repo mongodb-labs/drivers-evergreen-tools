51d7032c6e2c90fdf48750f040c81590964532b3
Steven Silvester <steven.silvester@ieee.org>
2023-12-12 09:52:31 -0600

    DRIVERS-2692 Update Auth OIDC Readme

--- .evergreen/auth_oidc/oidc_get_tokens.sh
+++ .evergreen/auth_oidc/oidc_get_tokens.sh
@@ -3,6 +3,9 @@
 # Get the set of OIDC tokens in the OIDC_TOKEN_DIR.
 #
 set -ex
+HERE=$(dirname $0)
+pushd $HERE
+
 if [ -z "$OIDC_TOKEN_DIR" ]; then
     if [ "Windows_NT" = "$OS" ]; then
         export OIDC_TOKEN_DIR=C:/Temp/tokens
@@ -12,4 +15,14 @@ if [ -z "$OIDC_TOKEN_DIR" ]; then
 fi
 mkdir -p $OIDC_TOKEN_DIR
 . ./activate-authoidcvenv.sh
+
+if [ ! -f "./secrets-export.sh" ]; then
+    AUTH_AWS="$HERE/../auth_aws"
+    set -x
+    echo "Getting oidc secrets"
+    python $AUTH_AWS/setup_secrets.py drivers/oidc
+    echo "Got secrets"
+fi
+
+source ./secrets-export.sh
 python oidc_get_tokens.py
