3659e5941317a6628c73cee9e207b6338143c206
Steven Silvester <steven.silvester@ieee.org>
2023-12-11 19:33:16 -0600

    clean up and handle load balancer

--- .evergreen/docker/run-client.sh
+++ .evergreen/docker/run-client.sh
@@ -8,6 +8,7 @@ set -eu
 # Docker related variables.
 IMAGE=${TARGET_IMAGE:-ubuntu20.04}
 PLATFORM=${DOCKER_PLATFORM:-}
+# e.g. --platform linux/amd64
 
 # Mongo orchestration related variables.
 MONGODB_VERSION=${MONGODB_VERSION:-latest}
@@ -17,33 +18,36 @@ SKIP_CRYPT_SHARED_LIB=${SKIP_CRYPT_SHARED_LIB:-false}
 AUTH=${AUTH:-""}
 SSL=${SSL:=""}
 
-
-export ROOT_DRIVERS_TOOLS=/root/drivers-evergeen-tools
+# Internal variables.
+ROOT_DRIVERS_TOOLS=/root/drivers-evergeen-tools
 MONGODB_BINARIES="ROOT_DRIVERS_TOOLS/.evergreen/docker/$IMAGE/mongodb/bin"
 
+# Build up the arguments.
 ARGS="$PLATFORM --rm -i"
-ARGS="$ARGS -e MONGODB_VERSION=$MONGODB_VERSION -e TOPOLOGY=$TOPOLOGY"
-ARGS="$ARGS -e SSL=$SSL -e AUTH=$AUTH"
-ARGS="$ARGS -e MONGODB_BINARIES=$MONGODB_BINARIES"
-ARGS="$ARGS -e CRYPT_SHARED_LIB_PATH=$MONGODB_BINARIES/mongosh_crypt_v1.so"
-ARGS="$ARGS -e ORCHESTRATION_FILE=$ORCHESTRATION_FILE"
-ARGS="$ARGS -e SKIP_CRYPT_SHARED_LIB=$SKIP_CRYPT_SHARED_LIB"
-ARGS="$ARGS -e DRIVERS_TOOLS="
+ARGS+=" -e MONGODB_VERSION=$MONGODB_VERSION -e TOPOLOGY=$TOPOLOGY"
+ARGS+=" -e SSL=$SSL -e AUTH=$AUTH"
+ARGS+=" -e MONGODB_BINARIES=$MONGODB_BINARIES"
+ARGS+=" -e CRYPT_SHARED_LIB_PATH=$MONGODB_BINARIES/mongosh_crypt_v1.so"
+ARGS+=" -e ORCHESTRATION_FILE=$ORCHESTRATION_FILE"
+ARGS+=" -e SKIP_CRYPT_SHARED_LIB=$SKIP_CRYPT_SHARED_LIB"
+ARGS+=" -e DRIVERS_TOOLS="
 
 # Ensure host.docker.internal is available on MacOS.
 if [ "$(uname -s)" = "Darwin" ]; then
-    ARGS="$ARGS -e MONGODB_URI=mongodb://host.docker.internal"
+    ARGS+=" -e MONGODB_URI=mongodb://host.docker.internal"
 fi
 
 # Ensure host network is available on Linux.
 if [ "$(uname -s)" = "Linux" ]; then
-    ARGS="$ARGS --network=host"
+    ARGS+=" --network=host"
 fi
 
 # If there is a tty, add the -t arg.
-test -t 1 && ARGS="-t $ARGS"
+test -t 1 && ARGS+=" -t"
 
-ARGS="$ARGS -v `pwd`:/src"
-ARGS="$ARGS -v $DRIVERS_TOOLS:/root/drivers-evergreen-tools"
+# Map the cwd to /src and map in DRIVERS_TOOLS.
+ARGS+=" -v `pwd`:/src"
+ARGS+=" -v $DRIVERS_TOOLS:/root/drivers-evergreen-tools"
 
+# Launch client docker container.
 docker run $ARGS $@
