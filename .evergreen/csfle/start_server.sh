8d5ab18dc1de33194593df3533229721e8d9616e
Steven Silvester <steven.silvester@ieee.org>
2023-12-20 19:34:45 -0600

    fix windows support

--- .evergreen/csfle/start_server.sh
+++ .evergreen/csfle/start_server.sh
@@ -11,13 +11,16 @@ pushd $SCRIPT_DIR
 # The -u options forces the stdout and stderr streams to be unbuffered.
 # TMPDIR is required to avoid "AF_UNIX path too long" errors.
 TMPDIR=$(mktemp -u) python -u kms_kmip_server.py --ca_file ../x509gen/ca.pem --cert_file ../x509gen/server.pem  --port 5698 &
+echo "$!" > kmip_pids.pd
+sleep 1
 python -u kms_http_server.py --ca_file ../x509gen/ca.pem --cert_file ../x509gen/expired.pem --port 8000 &
+echo "\n$!" >> kmip_pids.pd
 sleep 1
 python -u kms_http_server.py --ca_file ../x509gen/ca.pem --cert_file ../x509gen/wrong-host.pem --port 8001 &
+echo "\n$!" >> kmip_pids.pd
 sleep 1
 python -u kms_http_server.py --ca_file ../x509gen/ca.pem --cert_file ../x509gen/server.pem --port 8002 --require_client_cert &
+echo "\n$!" >> kmip_pids.pd
 sleep 1
-
-pgrep -f "kms_...._server.py" > kmip_pids.pid
 
 python -u kms_kmip_client.py
