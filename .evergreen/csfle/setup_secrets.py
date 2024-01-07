4a9e7a6dff036806de3c887ea3b8b49add658941
Steven Silvester <steven.silvester@ieee.org>
2024-01-07 16:49:23 -0600

    cleanup

--- .evergreen/csfle/setup_secrets.py
+++ .evergreen/csfle/setup_secrets.py
@@ -16,14 +16,14 @@ print("Getting CSFLE temp creds")
 client = boto3.client('sts')
 credentials = client.get_session_token()["Credentials"]
 
-with open('secrets-export.sh', 'ab') as fid:
-    fid.write(f'\nexport CSFLE_AWS_TEMP_ACCESS_KEY_ID="{credentials["AccessKeyId"]}"'.encode('utf8'))
-    fid.write(f'\nexport CSFLE_AWS_TEMP_SECRET_ACCESS_KEY="{credentials["SecretAccessKey"]}"'.encode('utf8'))
-    fid.write(f'\nexport CSFLE_AWS_TEMP_SESSION_TOKEN="{credentials["SessionToken"]}"'.encode('utf8'))
+with open('secrets-export.sh', 'a') as fid:
+    fid.write(f'\nexport CSFLE_AWS_TEMP_ACCESS_KEY_ID="{credentials["AccessKeyId"]}"')
+    fid.write(f'\nexport CSFLE_AWS_TEMP_SECRET_ACCESS_KEY="{credentials["SecretAccessKey"]}"')
+    fid.write(f'\nexport CSFLE_AWS_TEMP_SESSION_TOKEN="{credentials["SessionToken"]}"')
     for key in ['AWS_ACCESS_KEY_ID', 'AWS_SECRET_ACCESS_KEY', 'AWS_DEFAULT_REGION', 
                 'AWS_SESSION_TOKEN', 'CSFLE_TLS_CA_FILE', 'CSFLE_TLS_CERT_FILE',
                 'CSFLE_TLS_CLIENT_CERT_FILE']:
-        fid.write(f'\nexport {key}="{os.environ[key]}"'.encode('utf8'))
-    fid.write('\n'.encode('utf8'))
+        fid.write(f'\nexport {key}="{os.environ[key]}"')
+    fid.write('\n')
 
 print("Getting CSFLE temp creds...done")
