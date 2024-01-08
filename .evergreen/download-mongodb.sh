0375ee80a5fbddea9d2736bce923bf74408652fc
Steven Silvester <steven.silvester@ieee.org>
2024-01-08 14:45:17 -0600

    fix invocation

--- .evergreen/download-mongodb.sh
+++ .evergreen/download-mongodb.sh
@@ -680,7 +680,7 @@ download_and_extract_package ()
    if [ -n "$MONGODB_BINARY_ROOT" ]; then
       cd $MONGODB_BINARY_ROOT
    else
-      SCRIPT_DIR=$(dirname ${BASH_SOURCE[0]})
+      SCRIPT_DIR=$(dirname ${BASH_SOURCE:-$0})
       . $SCRIPT_DIR/handle-paths.sh
       cd $DRIVERS_TOOLS
    fi
@@ -706,7 +706,7 @@ download_and_extract_mongosh ()
    if [ -n "$MONGODB_BINARY_ROOT" ]; then
       cd $MONGODB_BINARY_ROOT
    else
-      SCRIPT_DIR=$(dirname ${BASH_SOURCE[0]})
+      SCRIPT_DIR=$(dirname ${BASH_SOURCE:-$0})
       . $SCRIPT_DIR/handle-paths.sh
       cd $DRIVERS_TOOLS
    fi
@@ -739,7 +739,7 @@ download_and_extract ()
       download_and_extract_mongosh "$MONGOSH_DOWNLOAD_URL" "$EXTRACT_MONGOSH"
    fi
 
-   SCRIPT_DIR=$(dirname ${BASH_SOURCE[0]})
+   SCRIPT_DIR=$(dirname ${BASH_SOURCE:-$0})
    . $SCRIPT_DIR/handle-paths.sh
 
    if [ ! -z "${INSTALL_LEGACY_SHELL:-}" -a ! -e $DRIVERS_TOOLS/mongodb/bin/mongo -a ! -e $DRIVERS_TOOLS/mongodb/bin/mongo.exe ]; then
