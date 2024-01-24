8d501b10d02280243d15cb82010d5b8c4fb9dec4
Steven Silvester <steven.silvester@ieee.org>
2024-01-24 06:22:58 -0600

    address review

--- .evergreen/csfle/start_servers.sh
+++ .evergreen/csfle/start_servers.sh
@@ -30,19 +30,19 @@ echo "Starting KMIP Server...done."
 sleep 1
 
 echo "Starting HTTP Server 1..."
-python -u kms_http_server.py --ca_file ../x509gen/ca.pem --cert_file ../x509gen/expired.pem --port 8000 > http1.log 2>&1 &
+python -u kms_http_server.py --ca_file ../x509gen/ca.pem --cert_file ../x509gen/expired.pem --port 9000 > http1.log 2>&1 &
 echo "$!" >> kmip_pids.pid
 echo "Starting HTTP Server 1...done."
 sleep 1
 
 echo "Starting HTTP Server 2..."
-python -u kms_http_server.py --ca_file ../x509gen/ca.pem --cert_file ../x509gen/wrong-host.pem --port 8001 > http2.log 2>&1 &
+python -u kms_http_server.py --ca_file ../x509gen/ca.pem --cert_file ../x509gen/wrong-host.pem --port 9001 > http2.log 2>&1 &
 echo "$!" >> kmip_pids.pid
 echo "Starting HTTP Server 2...done."
 sleep 1
 
 echo "Starting HTTP Server 3..."
-python -u kms_http_server.py --ca_file ../x509gen/ca.pem --cert_file ../x509gen/server.pem --port 8002 --require_client_cert > http3.log 2>&1 &
+python -u kms_http_server.py --ca_file ../x509gen/ca.pem --cert_file ../x509gen/server.pem --port 9002 --require_client_cert > http3.log 2>&1 &
 echo "$!" >> kmip_pids.pid
 echo "Starting HTTP Server 3...done."
 sleep 1
