0e213923ca0673b44b6bed2f770a6fedfa610817
Steven Silvester <steven.silvester@ieee.org>
2024-02-04 21:17:31 -0600

    fix atlas setup

--- .evergreen/atlas/setup-atlas-cluster.sh
+++ .evergreen/atlas/setup-atlas-cluster.sh
@@ -48,7 +48,7 @@ execution
 # Ensure that all variables required to run the test are set, otherwise throw
 # an error.
 for VARNAME in ${VARLIST[*]}; do
-[[ -z "${!VARNAME}" ]] && echo "ERROR: $VARNAME not set" && exit 1;
+[[ -z "${VARNAME}" ]] && echo "ERROR: $VARNAME not set" && exit 1;
 done
 
 # Set up the cluster variables.
