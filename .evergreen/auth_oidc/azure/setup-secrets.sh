5399271c17222b45527fcb15a238fa24436d0e51
Steven Silvester <steven.silvester@ieee.org>
2024-02-06 05:45:05 -0600

    fix path

--- .evergreen/auth_oidc/azure/setup-secrets.sh
+++ .evergreen/auth_oidc/azure/setup-secrets.sh
@@ -3,7 +3,7 @@ set -o errexit  # Exit the script with error if any of the commands fail
 set -x
 
 SCRIPT_DIR=$(dirname ${BASH_SOURCE[0]})
-. $SCRIPT_DIR/../handle-paths.sh
+. $SCRIPT_DIR/../../handle-paths.sh
 pushd $SCRIPT_DIR/..
 . $SCRIPT_DIR/../../secrets_handling/setup-secrets.sh drivers/azureoidc
 . ./activate-authoidcvenv.sh
