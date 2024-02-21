fb287da8e860fe931aad4fce504ce65c28dd9351
Steven Silvester <steve.silvester@mongodb.com>
2024-02-21 12:53:48 -0600

    fix path

--- .evergreen/auth_oidc/azure/teardown.sh
+++ .evergreen/auth_oidc/azure/teardown.sh
@@ -6,6 +6,6 @@ set -o errexit  # Exit the script with error if any of the commands fail
 SCRIPT_DIR=$(dirname ${BASH_SOURCE[0]})
 . $SCRIPT_DIR/handle-paths.sh
 
-if [ -f "./setup-secrets.sh" ]; then
+if [ -f "$SCRIPT_DIR/setup-secrets.sh" ]; then
     $SCRIPT_DIR/delete-vm.sh
 fi
