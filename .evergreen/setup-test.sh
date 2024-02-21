8f027281410fa2d6329cc5629a8af070f6998382
Steven Silvester <steve.silvester@mongodb.com>
2024-02-21 13:52:23 -0600

    Add setup script and update evg config

---
+++ .evergreen/setup-test.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+# Handle proper teardown of all assets and services created by drivers-evergreen-tools.
+
+set -o errexit  # Exit the script with error if any of the commands fail
+
+# Set up the mongo orchestrationn config.
+echo "{ \"releases\": { \"default\": \"$MONGODB_BINARIES\" }}" > $MONGO_ORCHESTRATION_HOME/orchestration.config
+
+# Copy client certificate because symlinks do not work on Windows.
+cp ${DRIVERS_TOOLS}/.evergreen/x509gen/client.pem ${MONGO_ORCHESTRATION_HOME}/lib/client.pem
+
+# Create failing test result file.
+echo '{"results": [{ "status": "FAIL", "test_file": "Build", "log_raw": "No test-results.json found was created"  } ]}' > ${PROJECT_DIRECTORY}/test-results.json
+
+# Handle absolute paths.
+for filename in $(find ${DRIVERS_TOOLS} -name \*.json); do
+    perl -p -i -e "s|ABSOLUTE_PATH_REPLACEMENT_TOKEN|${DRIVERS_TOOLS}|g" $filename
+done
+
+# Install project dependencies.
+if [ -f "$PROJECT_DIRECTORY/.evergreen/install-dependencies.sh" ]; then
+    bash "$PROJECT_DIRECTORY/.evergreen/install-dependencies.sh"
+fi
--- .evergreen/teardown-all.sh
+++ .evergreen/teardown-all.sh
@@ -8,24 +8,34 @@ SCRIPT_DIR=$(dirname ${BASH_SOURCE[0]})
 
 pushd $SCRIPT_DIR
 
-if [ -n "${MONGO_ORCHESTRATION_HOME:-}" ]; then
-    bash ./stop-orchestration.sh
+# Stop orchestration if it is running.
+if [ -f "${MONGO_ORCHESTRATION_HOME}/server.log" ]; then
+    # Purposely use sh here to ensure backwards compatibility.
+    sh ${DRIVERS_TOOLS}/.evergreen/stop-orchestration.sh
     # Consolidate the logs into the log directory.
     find $MONGO_ORCHESTRATION_HOME -name \*.log -exec sh -c 'x="{}"; mv $x ./log_dir/$(basename $(dirname $x))_$(basename $x)' \;
 fi
 
-# Remove all Docker images
+# Stop the load balancer.
+if [ -f "$DRIVERS_TOOLS/haproxy.conf" ]; then
+    bash /.evergreen/run-load-balancer.sh stop
+fi
+
+# Clean up docker
 DOCKER=$(command -v docker) || true
 if [ -n "$DOCKER" ]; then
+    # Kill all containers
+    docker rm $(docker ps -a -q)&> /dev/null || true
+    # Remove all images
     docker rmi -f $(docker images -a -q) &> /dev/null || true
 fi
 
 # Execute all available teardown scripts.
 find . -name "teardown.sh" -exec bash {} \;
 
-# Move all child log files into $DRIVERS_TOOLS/logs.tar.gz
+# Move all child log files into $DRIVERS_TOOLS/test_logs.tar.gz
 find . -name \*.log -exec sh -c 'x="{}"; mv $x ./log_dir/$(basename $(dirname $x))_$(basename $x)' \;
-tar zcvf $DRIVERS_TOOLS/logs.tar.gz -C log_dir/ .
+tar zcvf $DRIVERS_TOOLS/test_logs.tar.gz -C log_dir/ .
 rm -rf log_dir
 
 popd
