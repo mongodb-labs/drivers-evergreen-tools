39f4ce7f958d351b9bf44be7cded2a7656dc1c89
Steven Silvester <steve.silvester@mongodb.com>
2024-02-24 09:37:06 -0600

    finish test and add evg test

---
+++ .evergreen/auth_oidc/gcp/run-driver-test.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+set -o errexit
+set -o pipefail
+set -o nounset
+
+SCRIPT_DIR=$(dirname ${BASH_SOURCE[0]})
+. $SCRIPT_DIR/../../handle-paths.sh
+pushd $SCRIPT_DIR
+
+# Check for inputs.
+if [ -z "${AZUREOIDC_DRIVERS_TAR_FILE:-}" ] || \
+   [ -z "${AZUREOIDC_TEST_CMD:-}" ]; then
+    echo "Please set the following required environment variables"
+    echo " AZUREOIDC_DRIVERS_TAR_FILE"
+    echo " AZUREOIDC_TEST_CMD"
+    exit 1
+fi
+
+# Read in the env variables.
+source ./env.sh
+
+# Set up variables.
+export AZUREKMS_RESOURCEGROUP=$AZUREOIDC_RESOURCEGROUP
+export AZUREKMS_VMNAME=$AZUREOIDC_VMNAME
+export AZUREKMS_PRIVATEKEYPATH=$SCRIPT_DIR/keyfile
+
+# Set up the remote driver checkout.
+DRIVER_TARFILE_BASE=$(basename ${AZUREOIDC_DRIVERS_TAR_FILE})
+AZUREKMS_SRC=${AZUREOIDC_DRIVERS_TAR_FILE} \
+AZUREKMS_DST="~/" \
+  $SCRIPT_DIR/../../csfle/azurekms/copy-file.sh
+echo "Copying files ... end"
+echo "Untarring file ... begin"
+AZUREKMS_CMD="tar xf ${DRIVER_TARFILE_BASE}" \
+  $SCRIPT_DIR/../../csfle/azurekms/run-command.sh
+echo "Untarring file ... end"
+
+# Run the driver test.
+AZUREKMS_CMD="${AZUREOIDC_TEST_CMD}" \
+    $SCRIPT_DIR/../../csfle/azurekms/run-command.sh
--- .evergreen/auth_oidc/gcp/run-test.sh
+++ .evergreen/auth_oidc/gcp/run-test.sh
@@ -4,11 +4,7 @@ set -o pipefail
 
 echo "Installing dependencies ... begin"
 sudo apt-get update
-# Install dependencies.
 sudo apt-get -y -o DPkg::Lock::Timeout=-1 install git python3-pip python3-venv
-echo "Installing dependencies ... end"
-
-# Run the Python Driver Self Test
 git clone https://github.com/mongodb/mongo-python-driver
 pushd mongo-python-driver
 python3 -m venv .venv
@@ -17,6 +13,8 @@ pip install -U -q pip
 pip install -U -q requests setuptools
 python setup.py install --no_ext
 popd
+echo "Installing dependencies ... end"
+
+# Run the Python Driver Self Test
 source secrets-export.sh
 python test.py
-popd
