18a41f51a0b6fbe864072002d1ff65f2a7ba8c8f
Steven Silvester <steve.silvester@mongodb.com>
2024-02-25 12:55:55 -0600

    clean up logs

--- .evergreen/teardown-all.sh
+++ .evergreen/teardown-all.sh
@@ -32,14 +32,14 @@ fi
 find . -name "teardown.sh" -exec bash {} \;
 
 # Move all child log files into $DRIVERS_TOOLS/.evergreen/test_logs.tar.gz.
-export LOG_DIR=$(mktemp -d)
+LOG_DIR=$(mktemp -d)
 if [ "Windows_NT" = "${OS:-}" ]; then # Magic variable in cygwin
     LOG_DIR=$(cygpath -m $LOG_DIR)
 fi
-# Collapse the file path into the file name.
+# Prepend the parent directory name to the file name.
 find "$(pwd -P)" -name \*.log -exec sh -c 'x="{}"; cp $x $LOG_DIR/$(basename $(dirname $x))_$(basename $x)' \;
-# Handle files in this directory.
-find $LOG_DIR -name '.evergreen_*' -exec sh -c 'x="{}"; mv $x ${x/.evergreen_/}' \;
+# Handle files from this directory.
+find $LOG_DIR -name \.evergreen_\* -exec sh -c 'x="{}"; mv $x ${x/.evergreen_/}' \;
 # Slurp into a tar file.
 tar zcvf $(pwd -P)/test_logs.tar.gz -C $LOG_DIR/ .
 
