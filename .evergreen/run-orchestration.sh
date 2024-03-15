a01a9fb5d14644c7f1538b536fa1d915db5543d5
Steven Silvester <steve.silvester@mongodb.com>
2024-03-14 21:48:07 -0500

    Fix client cert handling in run orchestration

--- .evergreen/run-orchestration.sh
+++ .evergreen/run-orchestration.sh
@@ -50,7 +50,7 @@ if [ -n "${MONGODB_BINARIES}" ]; then
 fi
 
 # Copy client certificate because symlinks do not work on Windows.
-echo "hello M=$MONGO_ORCHESTRATION_HOME D=$DRIVERS_TOOLS"
+mkdir -p ${MONGO_ORCHESTRATION_HOME}/lib
 cp ${DRIVERS_TOOLS}/.evergreen/x509gen/client.pem ${MONGO_ORCHESTRATION_HOME}/lib/client.pem
 
 get_distro
