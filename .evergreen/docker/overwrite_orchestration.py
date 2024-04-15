34c92c64ebd12b5fd33b3e4e9161019cd86da272
Steven Silvester <steve.silvester@mongodb.com>
2024-04-14 20:31:34 -0500

    traverse the data

--- .evergreen/docker/overwrite_orchestration.py
+++ .evergreen/docker/overwrite_orchestration.py
@@ -20,6 +20,8 @@ def traverse(root):
         if isinstance(value, (dict, list)):
             traverse(value)
 
+traverse(data)
+
 # Docker does not enable ipv6 by default.
 # https://docs.docker.com/config/daemon/ipv6/
 # We also need to use 0.0.0.0 instead of 127.0.0.1
