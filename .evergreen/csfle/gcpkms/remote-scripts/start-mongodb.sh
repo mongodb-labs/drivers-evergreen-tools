92650024c573d48c48318b980890e529d046bc6c
Steven Silvester <steve.silvester@mongodb.com>
2024-06-06 19:12:31 -0500

    restore clones

--- .evergreen/csfle/gcpkms/remote-scripts/start-mongodb.sh
+++ .evergreen/csfle/gcpkms/remote-scripts/start-mongodb.sh
@@ -4,7 +4,7 @@ set -o pipefail
 # Do not error on unset variables. run-orchestration.sh accesses unset variables.
 
 echo "Starting MongoDB server ... begin"
-git clone --branch DRIVERS-2415-6-jun https://github.com/blink1073/drivers-evergreen-tools
+git clone https://github.com/mongodb-labs/drivers-evergreen-tools
 export DRIVERS_TOOLS=$(pwd)/drivers-evergreen-tools
 export MONGO_ORCHESTRATION_HOME="$DRIVERS_TOOLS/.evergreen/orchestration"
 export MONGODB_BINARIES="$DRIVERS_TOOLS/mongodb/bin"
