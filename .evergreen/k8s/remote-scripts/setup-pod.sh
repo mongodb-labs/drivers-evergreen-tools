6413b6e7730f4508ef95d3d4a1814ff9959174a0
Steven Silvester <steve.silvester@mongodb.com>
2024-06-30 19:54:40 -0500

    fix apt-get output

--- .evergreen/k8s/remote-scripts/setup-pod.sh
+++ .evergreen/k8s/remote-scripts/setup-pod.sh
@@ -2,11 +2,9 @@
 set -eux
 
 echo "Installing dependencies ... begin"
-# Make apt-get non-interactive.
-echo "debconf debconf/frontend select noninteractive" | debconf-set-selections
 apt-get -qq update
 # Same dependencies used in KMS testing.
-apt-get -qq -y -o DPkg::Lock::Timeout=-1 install libcurl4 libgssapi-krb5-2 libldap-2.4-2 libwrap0 libsasl2-2 \
+DEBIAN_FRONTEND=noninteractive apt-get -qq -y -o DPkg::Lock::Timeout=-1 install libcurl4 libgssapi-krb5-2 libldap-2.4-2 libwrap0 libsasl2-2 \
     libsasl2-modules-gssapi-mit snmp openssl liblzma5 \
     python3-pip python3.9-venv git sudo
 echo "Installing dependencies ... end"
