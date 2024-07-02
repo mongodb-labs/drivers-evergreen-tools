543a32822149c188c05c2561302c68e14ccb1f5c
Steven Silvester <steve.silvester@mongodb.com>
2024-07-02 10:18:02 -0500

    fix gcloud download

--- .evergreen/ensure-binary.sh
+++ .evergreen/ensure-binary.sh
@@ -42,7 +42,7 @@ case $NAME in
     esac
   ;;
   gcloud)
-    SUBPATH="google-cloud-sdk/bin/gcloud"
+    SUBPATH="bin/gcloud"
     BASE="https://dl.google.com/dl/cloudsdk/channels/rapid/downloads"
     case "$OS_NAME-$MARCH" in
         linux-x86_64)
