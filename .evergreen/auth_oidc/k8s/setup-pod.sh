ed168b70b5bb61a062719bf3f4ccff269ca04ecd
Steven Silvester <steve.silvester@mongodb.com>
2024-07-12 11:56:41 -0500

    fix handling of variant

--- .evergreen/auth_oidc/k8s/setup-pod.sh
+++ .evergreen/auth_oidc/k8s/setup-pod.sh
@@ -6,7 +6,7 @@ SCRIPT_DIR=$(dirname ${BASH_SOURCE[0]})
 pushd $SCRIPT_DIR
 
 # Set the current K8S_VARIANT.
-K8S_VARIANT=$1
+K8S_VARIANT=${K8S_VARIANT:-$1}
 echo "export K8S_VARIANT=$K8S_VARIANT" >> secrets-export.sh
 VARIANT=$(echo "$K8S_VARIANT" | tr '[:upper:]' '[:lower:]')
 VARIANT_DIR=$DRIVERS_TOOLS/.evergreen/k8s/$VARIANT
