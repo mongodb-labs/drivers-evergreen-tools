2b21e78656660d0c9c2d7b3c84bca5de8f261493
Steven Silvester <steven.silvester@ieee.org>
2024-08-27 19:05:46 -0500

    GODRIVER-3311 Fix AzureKMS and Atlas Startup Scripts (#478)

--- .evergreen/atlas/setup.sh
+++ .evergreen/atlas/setup.sh
@@ -9,3 +9,6 @@ SCRIPT_DIR=$(dirname ${BASH_SOURCE[0]})
 
 source $SCRIPT_DIR/secrets-export.sh
 . ${DRIVERS_TOOLS}/.evergreen/check-connection.sh
+
+# Clean up binary directory.
+rm -rf ${DRIVERS_TOOLS}/mongodb
