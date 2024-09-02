dec2b989e3da482c1f63d6593d3fed59d713ff22
Steven Silvester <steve.silvester@mongodb.com>
2024-09-02 16:47:57 -0500

    try again

--- .evergreen/docker/ubuntu20.04/test-entrypoint.sh
+++ .evergreen/docker/ubuntu20.04/test-entrypoint.sh
@@ -2,7 +2,7 @@
 set -eu
 
 bash /root/base-entrypoint.sh
-source $DRIVERS_TOOLS/.evergreen/mo-expansion.sh
+source $DRIVERS_TOOLS/mo-expansion.sh
 URI="mongodb://127.0.0.1:27017/?serverSelectionTimeoutMS=10000"
 $MONGODB_BINARIES/mongosh $URI --eval "db.runCommand({\"ping\":1})"
 echo "Test complete!"
