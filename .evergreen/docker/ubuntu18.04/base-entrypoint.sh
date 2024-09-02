c8396b1df9d5e47e9c0e7e122b7d87b3673cfd41
Steven Silvester <steve.silvester@mongodb.com>
2024-09-02 16:42:06 -0500

    fix paths

--- .evergreen/docker/ubuntu18.04/base-entrypoint.sh
+++ .evergreen/docker/ubuntu18.04/base-entrypoint.sh
@@ -7,7 +7,7 @@ make run-server
 
 # Preserve host permissions of files we have created.
 cd $DRIVERS_TOOLS
-files=(results.json uri.txt .evergreen/mongo_crypt_v1.so .evergreen/mo-expansion.yml)
+files=(results.json uri.txt mongo_crypt_v1.so mo-expansion.yml)
 chown --reference=action.yml "${files[@]}"
 chmod --reference=action.yml "${files[@]}"
 
