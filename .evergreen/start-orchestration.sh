7be073fb76584df8353c974463878431c3a4e90e
Steven Silvester <steve.silvester@mongodb.com>
2024-09-04 14:26:27 -0500

    update comment

--- .evergreen/start-orchestration.sh
+++ .evergreen/start-orchestration.sh
@@ -35,8 +35,8 @@ venvcreate "${PYTHON:?}" venv
 echo "Creating virtual environment 'venv'... done."
 
 # Install from github to get the latest mongo-orchestration, fall back on published wheel.
-# The fallback was added to accommodate RHEL 7.0, whose version of glibc does not support the
-# hatchling backend used by mongo-orchestration.
+# The fallback was added to accommodate versions of Python 3 for which there is no compatible version
+# of the hatchling backend used by mongo-orchestration.
 python -m pip install -q --upgrade 'https://github.com/mongodb/mongo-orchestration/archive/master.tar.gz' || python -m pip install -q --upgrade mongo-orchestration
 python -m pip list
 cd $DRIVERS_TOOLS
