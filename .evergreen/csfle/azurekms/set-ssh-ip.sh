8cfc3e91dd854a61bcced2b182e772232d10aa97
Kevin Albertson <kevin.albertson@mongodb.com>
2024-09-26 10:38:10 -0400

    add set-ssh-ip.sh to add current IP
    Needed since Azure KMS task group may run scripts across multiple Evergreen hosts.

---
+++ .evergreen/csfle/azurekms/set-ssh-ip.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+# set-ssh-ip.sh adds the current IP to an already-created VM.
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+# Get DRIVERS_TOOLS path.
+SCRIPT_DIR=$(dirname "${BASH_SOURCE[0]}")
+. "$SCRIPT_DIR"/../../handle-paths.sh
+
+VARLIST=(
+    AZUREKMS_RESOURCEGROUP
+    AZUREKMS_VMNAME
+    AZUREKMS_PRIVATEKEYPATH
+)
+
+# Ensure that all variables required to run the test are set, otherwise throw
+# an error.
+for VARNAME in "${VARLIST[@]}"; do
+  [[ -z "${!VARNAME:-}" ]] && echo "ERROR: $VARNAME not set" && exit 1;
+done
+
+EXTERNAL_IP=$(curl -s http://whatismyip.akamai.com/)
+
+echo "Adding current IP ($EXTERNAL_IP) to Azure Virtual Machine ... begin"
+az network nsg rule update \
+    --only-show-errors \
+    --name "$AZUREKMS_VMNAME-nsg-rule" \
+    --nsg-name "$AZUREKMS_VMNAME-nsg" \
+    --resource-group "$AZUREKMS_RESOURCEGROUP" \
+    --source-address-prefixes "$EXTERNAL_IP"
+
+IP=$(az vm show --show-details --resource-group "$AZUREKMS_RESOURCEGROUP" --name "$AZUREKMS_VMNAME" --query publicIps -o tsv)
+
+"$DRIVERS_TOOLS/.evergreen/retry-with-backoff.sh" ssh -o ConnectTimeout=10 -o StrictHostKeyChecking=no azureuser@"$IP" -i "$AZUREKMS_PRIVATEKEYPATH" "echo 'hi' > /dev/null"
+
+echo "Adding current IP ($EXTERNAL_IP) to Azure Virtual Machine ... end"
