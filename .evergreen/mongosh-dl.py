d7409af5a9417cc5fe6c785fbcb3102f2e191109
Steven Silvester <steve.silvester@mongodb.com>
2024-11-01 22:24:54 -0500

    try debian12 again

--- .evergreen/mongosh-dl.py
+++ .evergreen/mongosh-dl.py
@@ -70,8 +70,6 @@ def _download(out_dir: Path, version: str, target: str,
         if sys.platform == 'linux' and arch in ["x64", "arm64"]:
             if ssl.OPENSSL_VERSION_INFO[0] == 3:
                 suffix = "-openssl3.tgz"
-            else:
-                suffix = "-openssl11.tgz"
     else:
         suffix = ".zip"
     dl_url = f"https://downloads.mongodb.com/compass/mongosh-{version}-{target}-{arch}{suffix}"
