e4a41b6d92fcc4ed0a31f5e3d85d91569fa705b0
Steven Silvester <steve.silvester@mongodb.com>
2024-11-01 21:41:12 -0500

    fix test

--- .evergreen/tests/test-mongodl.sh
+++ .evergreen/tests/test-mongodl.sh
@@ -10,9 +10,9 @@ pushd $SCRIPT_DIR/..
 PYTHON=$(ensure_python3)
 echo "Using PYTHON: $PYTHON"
 mkdir mongodl_test
-$PYTHON mongodl.py --edition enterprise --version 7.0 --component archive --no-download
+$PYTHON mongodl.py --edition enterprise --version 7.0.5 --component archive --no-download
 if [ "${OS:-}" != "Windows_NT" ]; then
-  $PYTHON mongodl.py --edition enterprise --version 7.0.5 --component archive-debug --test
+  $PYTHON mongodl.py --edition enterprise --version 7.0 --component archive-debug --test
 fi
 $PYTHON mongodl.py --edition enterprise --version 7.0 --component cryptd --out $(pwd)/mongodl_test --strip-path-components 1
 $PYTHON mongosh-dl.py --no-download
