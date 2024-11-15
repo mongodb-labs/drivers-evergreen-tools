74a015627b65db3496750543c60ea6027eb00921
Steven Silvester <steve.silvester@mongodb.com>
2024-11-15 12:10:53 -0600

    try again

--- .evergreen/install-cli.sh
+++ .evergreen/install-cli.sh
@@ -47,7 +47,8 @@ if [ "Windows_NT" == "${OS:-}" ]; then
   TMP_DIR="$(mktemp -d)"
   PATH="$SCRIPT_DIR/venv/Scripts:$PATH"
   UV_TOOL_BIN_DIR=${TMP_DIR} uv tool install --force --editable .
-  for filename in "$TMP_DIR"/*; do
+  filenames=$(ls ${TMP_DIR})
+  for filename in $filenames; do
     mv $TMP_DIR/$filename "$1/${filename//.exe/}"
   done
   rm -rf $TMP_DIR
