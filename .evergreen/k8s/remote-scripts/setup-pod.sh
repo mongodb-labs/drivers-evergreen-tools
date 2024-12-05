9236d1edcae6350ac172a40d37a9d3a22932a7a1
Oleksandr Poliakov <31327136+sanych-sun@users.noreply.github.com>
2024-12-05 14:07:26 -0800

    Install curl in pod for k8s testing (#562)

--- .evergreen/k8s/remote-scripts/setup-pod.sh
+++ .evergreen/k8s/remote-scripts/setup-pod.sh
@@ -6,6 +6,6 @@ apt-get -qq update
 # Same dependencies used in KMS testing.
 export DEBIAN_FRONTEND=noninteractive
 apt-get -qq -y -o DPkg::Lock::Timeout=-1 install libcurl4 libgssapi-krb5-2 libldap-common libwrap0 libsasl2-2 \
-    libsasl2-modules-gssapi-mit snmp openssl liblzma5 \
+    libsasl2-modules-gssapi-mit snmp openssl liblzma5 curl \
     python3-pip python3.11-venv git sudo < /dev/null > /dev/null
 echo "Installing dependencies ... end"
