8feee453e4187e41b799c4b3efc8c90a367d1027
Steven Silvester <steve.silvester@mongodb.com>
2024-12-18 21:51:15 -0600

    fix docker

--- .evergreen/docker/ubuntu18.04/base-entrypoint.sh
+++ .evergreen/docker/ubuntu18.04/base-entrypoint.sh
@@ -10,7 +10,7 @@ make run-server
 
 # Preserve host permissions of files we have created.
 cd $DRIVERS_TOOLS
-files=(results.json uri.txt mo-expansion.yml)
+files=(results.json mo-expansion.yml)
 chown --reference=action.yml "${files[@]}"
 chmod --reference=action.yml "${files[@]}"
 
