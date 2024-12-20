64e19d38b3c392aec1c3ce67ac34aa1e1ca48abc
Steven Silvester <steve.silvester@mongodb.com>
2024-12-19 18:52:24 -0600

    fix client runner

--- .evergreen/docker/run-client.sh
+++ .evergreen/docker/run-client.sh
@@ -26,18 +26,13 @@ SKIP_CRYPT_SHARED_LIB=${SKIP_CRYPT_SHARED_LIB:-false}
 AUTH=${AUTH:-""}
 SSL=${SSL:-""}
 
-# Internal variables.
-MONGODB_BINARIES="/root/drivers-evergreen-tools/mongodb/bin"
-
 # Build up the arguments.
 ARGS="$PLATFORM --rm -i"
 ARGS+=" -e MONGODB_VERSION=$MONGODB_VERSION -e TOPOLOGY=$TOPOLOGY"
 ARGS+=" -e SSL=$SSL -e AUTH=$AUTH"
-ARGS+=" -e MONGODB_BINARIES=$MONGODB_BINARIES"
 ARGS+=" -e CRYPT_SHARED_LIB_PATH=$MONGODB_BINARIES/mongosh_crypt_v1.so"
 ARGS+=" -e ORCHESTRATION_FILE=$ORCHESTRATION_FILE"
 ARGS+=" -e SKIP_CRYPT_SHARED_LIB=$SKIP_CRYPT_SHARED_LIB"
-ARGS+=" -e DRIVERS_TOOLS=$ROOT_DRIVERS_TOOLS"
 
 # Ensure host.docker.internal is available on MacOS.
 if [ "$(uname -s)" = "Darwin" ]; then
@@ -54,7 +49,7 @@ test -t 1 && ARGS+=" -t"
 
 # Map the cwd to /src and map in DRIVERS_TOOLS.
 ARGS+=" -v `pwd`:/src"
-ARGS+=" -v $DRIVERS_TOOLS:/root/drivers-evergreen-tools"
+ARGS+=" -v $DRIVERS_TOOLS:/root/drivers-tools"
 
 # Launch client docker container.
 $DOCKER run $ARGS "$@"
