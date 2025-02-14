afbaa221f5d28ba9a97b60ad3443fe501abb8953
Steven Silvester <steve.silvester@mongodb.com>
2025-02-14 10:44:28 -0600

    Make output quieter

--- .evergreen/csfle/stop-servers.sh
+++ .evergreen/csfle/stop-servers.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Clean up CSFLE kmip servers
-set -ex
+set -eu
 
 SCRIPT_DIR=$(dirname ${BASH_SOURCE[0]})
 . $SCRIPT_DIR/../handle-paths.sh
@@ -9,7 +9,7 @@ pushd $SCRIPT_DIR
 rm -f pykmip.db
 if [ -f "kmip_pids.pid" ]; then
   while read p; do
-    kill -9 "$p" || true
+    kill -9 "$p" 2> /dev/null || true
   done <kmip_pids.pid
   rm kmip_pids.pid
 fi
