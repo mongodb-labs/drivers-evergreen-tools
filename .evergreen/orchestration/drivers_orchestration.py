a81621b98c06789fcad410b9893ae8a3631d8794
Steven Silvester <steve.silvester@mongodb.com>
2025-02-17 14:39:40 -0600

    add mongodb uri

--- .evergreen/orchestration/drivers_orchestration.py
+++ .evergreen/orchestration/drivers_orchestration.py
@@ -317,6 +317,8 @@ def run(opts):
     uri = resp.get("mongodb_auth_uri", resp["mongodb_uri"])
     expansion_yaml.touch()
     expansion_yaml.write_text(expansion_yaml.read_text() + f'\nMONGODB_URI: "{uri}"')
+    expansion_sh.touch()
+    expansion_sh.write_text(expansion_sh.read_text() + f'\nMONGODB_URI="{uri}"')
     uri_txt.write_text(uri)
     LOGGER.info(f"Cluster URI: {uri}")
 
