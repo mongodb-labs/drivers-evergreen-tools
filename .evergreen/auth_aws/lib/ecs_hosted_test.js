9ace3bd8ba6a3c39a36065c2028a92afbe43e8e7
Steven Silvester <steve.silvester@mongodb.com>
2025-02-18 12:34:14 -0600

    add both users

--- .evergreen/auth_aws/lib/ecs_hosted_test.js
+++ .evergreen/auth_aws/lib/ecs_hosted_test.js
@@ -7,6 +7,7 @@
 
 // This varies based on hosting ECS task as the account id and role name can vary
 const AWS_ACCOUNT_ARN = "arn:aws:sts::557821124784:assumed-role/ecsTaskExecutionRole/*";
+const AWS_ACCOUNT_ARN2 = "arn:aws:iam::557821124784:user/authtest_fargate_user";
 
 const external = Mongo().getDB("$external");
 const admin = Mongo().getDB("admin");
@@ -21,6 +22,8 @@ admin.auth("bob", "pwd123");
 
 console.log("Adding user:", AWS_ACCOUNT_ARN)
 external.runCommand({createUser: AWS_ACCOUNT_ARN, roles:[{role: 'read', db: "aws"}]});
+console.log("Adding user:", AWS_ACCOUNT_ARN2)
+external.runCommand({createUser: AWS_ACCOUNT_ARN2, roles:[{role: 'read', db: "aws"}]});
 
 // Try the auth function
 const testConn = new Mongo();
