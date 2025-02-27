0209b78d651337b672b4b09f4410398792da8653
Steven Silvester <steve.silvester@mongodb.com>
2025-02-27 16:39:54 -0600

    debug

--- .evergreen/auth_aws/teardown.sh
+++ .evergreen/auth_aws/teardown.sh
@@ -10,6 +10,8 @@ pushd $SCRIPT_DIR
 # If we've gotten credentials, ensure the instance profile is set.
 if [ -f secrets-export.sh ]; then
   . ./activate-authawsvenv.sh
+  set -x
+  env | grep "AWS_ACCESS_KEY_ID"
   echo "SOURCING SECRETS!"
   env | grep "AWS_ACCESS_KEY_ID"
   source secrets-export.sh
