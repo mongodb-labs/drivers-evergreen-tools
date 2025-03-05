2d3d7ffe7e9ee2ce2e8296732b7e16e0826242b1
Steven Silvester <steve.silvester@mongodb.com>
2025-03-05 14:30:15 -0600

    fix test

--- .evergreen/tests/test-aws.sh
+++ .evergreen/tests/test-aws.sh
@@ -15,11 +15,13 @@ bash aws_setup.sh assume-role
 cat test-env.sh | grep -q USER
 cat test-env.sh | grep -q PASS
 cat test-env.sh | grep -q SESSION_TOKEN
+# Ensure there is a password in the URI.
 cat test-env.sh | grep MONGODB_URI | grep -q "@"
 rm test-env.sh
 
 bash aws_setup.sh ec2
-cat test-env.sh | grep -q MONGODB_URI | grep -c -q "@"
+# Ensure there is no password in the URI.
+cat test-env.sh | grep -q MONGODB_URI | grep -v -q "@"
 rm test-env.sh
 
 bash aws_setup.sh regular
@@ -33,20 +35,20 @@ bash aws_setup.sh session-creds
 cat test-env.sh | grep -q AWS_ACCESS_KEY_ID
 cat test-env.sh | grep -q AWS_SECRET_ACCESS_KEY
 cat test-env.sh | grep -q AWS_SESSION_TOKEN
-cat test-env.sh | grep -q MONGODB_URI | grep -c -q "@"
+cat test-env.sh | grep -q MONGODB_URI | grep -v -q "@"
 rm test-env.sh
 
 bash aws_setup.sh env-creds
 cat test-env.sh | grep -q AWS_ACCESS_KEY_ID
 cat test-env.sh | grep -q AWS_SECRET_ACCESS_KEY
 cat test-env.sh | grep -c -q AWS_SESSION_TOKEN
-cat test-env.sh | grep -q MONGODB_URI | grep -c -q "@"
+cat test-env.sh | grep -q MONGODB_URI | grep -v -q "@"
 rm test-env.sh
 
 bash aws_setup.sh web-identity
 cat test-env.sh | grep -q AWS_WEB_IDENTITY_TOKEN_FILE
 cat test-env.sh | grep -q AWS_ROLE_ARN
-cat test-env.sh | grep -q MONGODB_URI | grep -c -q "@"
+cat test-env.sh | grep -q MONGODB_URI | grep -v -q "@"
 rm test-env.sh
 
 bash ./teardown.sh
