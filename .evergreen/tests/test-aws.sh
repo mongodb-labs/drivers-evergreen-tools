d8cd31fe05d084b53cea6e1fb3ab352279f7b228
Steven Silvester <steve.silvester@mongodb.com>
2025-03-05 14:47:10 -0600

    fix test

--- .evergreen/tests/test-aws.sh
+++ .evergreen/tests/test-aws.sh
@@ -27,7 +27,7 @@ rm test-env.sh
 bash aws_setup.sh regular
 cat test-env.sh | grep -q USER
 cat test-env.sh | grep -q PASS
-cat test-env.sh | grep -q -c SESSION_TOKEN
+cat test-env.sh | grep -v -q SESSION_TOKEN
 cat test-env.sh | grep MONGODB_URI | grep -q "@"
 rm test-env.sh
 
