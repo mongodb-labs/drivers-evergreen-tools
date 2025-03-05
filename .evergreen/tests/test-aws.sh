961c46f00b9fbe32fedb6186fb73936fdebabd77
Steven Silvester <steve.silvester@mongodb.com>
2025-03-05 14:24:04 -0600

    update test

--- .evergreen/tests/test-aws.sh
+++ .evergreen/tests/test-aws.sh
@@ -15,35 +15,38 @@ bash aws_setup.sh assume-role
 cat test-env.sh | grep -q USER
 cat test-env.sh | grep -q PASS
 cat test-env.sh | grep -q SESSION_TOKEN
-cat test-env.sh | grep -q MONGODB_URI
+cat test-env.sh | grep MONGODB_URI | grep -q "@"
 rm test-env.sh
 
 bash aws_setup.sh ec2
-cat test-env.sh | grep -q MONGODB_URI
+cat test-env.sh | grep -q MONGODB_URI | grep -c -q "@"
 rm test-env.sh
 
 bash aws_setup.sh regular
 cat test-env.sh | grep -q USER
 cat test-env.sh | grep -q PASS
-cat test-env.sh | grep -q MONGODB_URI
+cat test-env.sh | grep -q -c SESSION_TOKEN
+cat test-env.sh | grep MONGODB_URI | grep -q "@"
 rm test-env.sh
 
 bash aws_setup.sh session-creds
 cat test-env.sh | grep -q AWS_ACCESS_KEY_ID
 cat test-env.sh | grep -q AWS_SECRET_ACCESS_KEY
 cat test-env.sh | grep -q AWS_SESSION_TOKEN
-cat test-env.sh | grep -q MONGODB_URI
+cat test-env.sh | grep -q MONGODB_URI | grep -c -q "@"
 rm test-env.sh
 
 bash aws_setup.sh env-creds
 cat test-env.sh | grep -q AWS_ACCESS_KEY_ID
 cat test-env.sh | grep -q AWS_SECRET_ACCESS_KEY
-cat test-env.sh | grep -q MONGODB_URI
+cat test-env.sh | grep -c -q AWS_SESSION_TOKEN
+cat test-env.sh | grep -q MONGODB_URI | grep -c -q "@"
 rm test-env.sh
 
 bash aws_setup.sh web-identity
 cat test-env.sh | grep -q AWS_WEB_IDENTITY_TOKEN_FILE
 cat test-env.sh | grep -q AWS_ROLE_ARN
+cat test-env.sh | grep -q MONGODB_URI | grep -c -q "@"
 rm test-env.sh
 
 bash ./teardown.sh
