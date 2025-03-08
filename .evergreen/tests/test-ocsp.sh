2ad50d32605ae26d0a504be989bcf5718a521732
Steven Silvester <steve.silvester@mongodb.com>
2025-03-08 16:19:09 -0600

    fix option handling

--- .evergreen/tests/test-ocsp.sh
+++ .evergreen/tests/test-ocsp.sh
@@ -9,13 +9,16 @@ SCRIPT_DIR=$(dirname ${BASH_SOURCE[0]})
 if [[ $(uname -s) = "Linux" ]]; then
   ORCHESTRATION_FILE="ecdsa-basic-tls-ocsp-mustStaple.json"
   OCSP_ALGORITHM="ecdsa"
+  SERVER_TYPE="valid-delegate"
 else
   ORCHESTRATION_FILE="rsa-basic-tls-ocsp-disableStapling.json"
   OCSP_ALGORITHM="rsa"
+  SERVER_TYPE="valid"
 fi
 
 export ORCHESTRATION_FILE
 export OCSP_ALGORITHM
+export SERVER_TYPE
 
 # # Start a MongoDB server with ocsp enabled.
 SSL="ssl" make -C ${DRIVERS_TOOLS} run-server
@@ -23,14 +26,14 @@ SSL="ssl" make -C ${DRIVERS_TOOLS} run-server
 pushd $SCRIPT_DIR/../ocsp
 
 # # Start the ocsp server.
-SERVER_TYPE="valid" bash ./setup.sh
+bash ./setup.sh
 
 # Connect to the MongoDB server.
 echo "Connecting to server..."
 TLS_OPTS=("--tls --tlsCertificateKeyFile \"${DRIVERS_TOOLS}/.evergreen/ocsp/${OCSP_ALGORITHM}/server.pem\"")
 TLS_OPTS+=("--tlsCAFile \"${DRIVERS_TOOLS}/.evergreen/ocsp/${OCSP_ALGORITHM}/ca.pem\"")
-URI="mongodb://localhost:27017/&serverSelectionTimeoutMS=20000"
-$MONGODB_BINARIES/mongosh $URI "${TLS_OPTS[@]}" --eval "db.runCommand({\"ping\":1})"
+# shellcheck disable=SC2068
+$MONGODB_BINARIES/mongosh ${TLS_OPTS[@]} --eval "db.runCommand({\"ping\":1})"
 echo "Connecting to server... done."
 
 bash ./teardown.sh
