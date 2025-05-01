502a371216218a75049c41e9f9a6b2f53d3d7647
Steven Silvester <steven.silvester@ieee.org>
2025-05-01 12:29:32 -0500

    Update .evergreen/orchestration/setup.sh
    Co-authored-by: Ezra Chung <88335979+eramongodb@users.noreply.github.com>

--- .evergreen/orchestration/setup.sh
+++ .evergreen/orchestration/setup.sh
@@ -6,6 +6,7 @@ set -o errexit
 SCRIPT_DIR=$(dirname ${BASH_SOURCE[0]})
 . $SCRIPT_DIR/../handle-paths.sh
 
-# Install the clis in this folder and the parent folder.
+# Install CLIs into this directory (default path for $PROJECT_ORCHESTRATION_HOME
+# and $MONGO_ORCHESTRATION_HOME) and the parent directory ($DRIVERS_TOOLS).
 bash $SCRIPT_DIR/../install-cli.sh $SCRIPT_DIR/..
 bash $SCRIPT_DIR/../install-cli.sh $SCRIPT_DIR
