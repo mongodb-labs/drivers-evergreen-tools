4acb2319d319f2e8cf8bee38e91db6b13033e664
Oleksandr Poliakov <oleksandr.poliakov@mongodb.com>
2025-05-06 12:07:46 -0700

    investigate

--- .evergreen/install-cli.sh
+++ .evergreen/install-cli.sh
@@ -59,6 +59,9 @@ if ! command -v uv &>/dev/null; then
   esac
 fi
 
+echo =================
+echo "$DRIVERS_TOOLS"
+echo "$PATH"
 # ensure $DRIVERS_TOOLS/.bin is in $PATH
 if [[ ! $PATH =~ :?$DRIVERS_TOOLS/\.bin/?:? ]]; then
   echo Adding DRIVERS_TOOLS/.bin folder to the PATH variable
