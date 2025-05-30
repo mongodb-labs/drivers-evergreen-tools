9d228d6be738720e6e9b5c05fc81d710099c62ac
Steven Silvester <steve.silvester@mongodb.com>
2025-05-30 09:44:00 -0500

    debug

--- .evergreen/k8s/eks/teardown.sh
+++ .evergreen/k8s/eks/teardown.sh
@@ -6,6 +6,7 @@ SCRIPT_DIR=$(dirname ${BASH_SOURCE[0]})
 . $SCRIPT_DIR/../../handle-paths.sh
 
 echo "HELLO from teardown $SCRIPT_DIR"
+ls $SCRIPT_DIR
 if [ -f $SCRIPT_DIR/secrets-export.sh ]; then
   echo "Sourcing secrets"
   source $SCRIPT_DIR/secrets-export.sh
