77634064e187bbc6512cbeee8a8158676cb72fde
Steven Silvester <steve.silvester@mongodb.com>
2025-05-30 09:46:30 -0500

    debug

--- .evergreen/auth_aws/aws_setup.sh
+++ .evergreen/auth_aws/aws_setup.sh
@@ -35,6 +35,7 @@ if [ -f $SCRIPT_DIR/test-env.sh ]; then
     rm $SCRIPT_DIR/test-env.sh
 fi
 
+echo "AWS KEY?: $AWS_ACCESS_KEY_ID"
 PROJECT_DIRECTORY=$PROJECT_DIRECTORY python aws_tester.py "$@"
 
 # Remove any AWS creds that might be set in the parent env.
