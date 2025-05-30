2f1bc3d3af35fee9a8f1a785b64976f4fe54ceb4
Steven Silvester <steve.silvester@mongodb.com>
2025-05-30 09:31:34 -0500

    whitespace

--- .evergreen/auth_aws/aws_setup.sh
+++ .evergreen/auth_aws/aws_setup.sh
@@ -35,7 +35,7 @@ if [ -f $SCRIPT_DIR/test-env.sh ]; then
     rm $SCRIPT_DIR/test-env.sh
 fi
 
-PROJECT_DIRECTORY=$PROJECT_DIRECTORY  python aws_tester.py "$@"
+PROJECT_DIRECTORY=$PROJECT_DIRECTORY python aws_tester.py "$@"
 
 # Remove any AWS creds that might be set in the parent env.
 if [ $1 == "eks" ]; then
