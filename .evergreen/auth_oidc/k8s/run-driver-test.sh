ee0b03946e59c906890548cc3819a7d992fcffc6
Viacheslav Babanin <slav.babanin@mongodb.com>
2025-07-03 18:06:52 -0700

    Increase ephemeral storage and add diagnostics for k8s (#665)

--- .evergreen/auth_oidc/k8s/run-driver-test.sh
+++ .evergreen/auth_oidc/k8s/run-driver-test.sh
@@ -31,7 +31,26 @@ echo "Setting up driver test files... done."
 # Run the command.
 echo "Running the driver test command..."
 kubectl cp ./secrets-export.sh ${K8S_POD_NAME}:/tmp/test/secrets-export.sh
-kubectl exec ${K8S_POD_NAME} -- bash -c "cd /tmp/test && source secrets-export.sh && ${K8S_TEST_CMD}"
-echo "Running the driver test command... done."
 
+# Temporarily disable `set -e` for this command to capture the exit code
+set +e
+kubectl exec ${K8S_POD_NAME} -- bash -c "cd /tmp/test && source secrets-export.sh && ${K8S_TEST_CMD}"
+EXIT_CODE=$?  # Capture the exit code
+set -e  # Re-enable `set -e`
+if [ $EXIT_CODE -ne 0 ]; then
+   if [ $EXIT_CODE -eq 137 ]; then
+      echo -e "\n ERROR: Pod process was OOMKilled (Exit Code 137).\n"
+   else
+      echo -e "\n ERROR: Pod process failed with exit code $EXIT_CODE.\n"
+   fi
+
+    echo -e "\n Pod description:\n"
+    # Print pod details
+    kubectl describe pod ${K8S_POD_NAME}
+    echo -e "\n Pod logs:\n"
+    # Print pod logs (current still active or previous pod)
+    kubectl logs ${K8S_POD_NAME} || kubectl logs -p ${K8S_POD_NAME}
+    exit $EXIT_CODE
+fi
+echo "Running the driver test command... done."
 popd
