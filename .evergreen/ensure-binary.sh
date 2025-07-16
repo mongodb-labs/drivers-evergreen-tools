04a04c8d34188cb4e94350398d32450c85d83d6e
Steven Silvester <steve.silvester@mongodb.com>
2025-07-16 15:21:21 -0500

    DRIVERS-3223 Verify downloads for binaries

--- .evergreen/ensure-binary.sh
+++ .evergreen/ensure-binary.sh
@@ -26,7 +26,8 @@ fi
 
 _OS_NAME=$(uname -s | tr '[:upper:]' '[:lower:]')
 _MARCH=$(uname -m | tr '[:upper:]' '[:lower:]')
-_URL=""
+
+mkdir -p ${DRIVERS_TOOLS}/.bin
 
 case $_NAME in
   kubectl)
@@ -45,49 +46,55 @@ case $_NAME in
         darwin-arm64)
           _URL="$_BASE/darwin/arm64/kubectl"
         ;;
+        *)
+           echo "Unsupported for $_NAME: $_OS_NAME-$_MARCH"
+           return 1
+        ;;
     esac
+    pushd ${DRIVERS_TOOLS}/.bin
+    "$SCRIPT_DIR/retry-with-backoff.sh" curl -L -s -O $_URL
+    "$SCRIPT_DIR/retry-with-backoff.sh" curl -L -s -O "$_URL.sha256"
+    echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
+    chmod +x kubectl
   ;;
   gcloud)
     _BASE="https://dl.google.com/dl/cloudsdk/channels/rapid/downloads"
     case "$_OS_NAME-$_MARCH" in
        linux-x86_64)
           _URL="$_BASE/google-cloud-cli-linux-x86_64.tar.gz"
+          _SHA_SUM=1ec7d351de14277c00c430f9fb939255c4b63940417da2f24ff6413a9e221c2f
         ;;
         linux-aarch64)
           _URL="$_BASE/google-cloud-cli-linux-arm.tar.gz"
+          _SHA_SUM=e6153461e3154ebce61d35b73005bdd14a0ecacd42e5008f66e25b4ad231e5c9
         ;;
         darwin-x86_64)
           _URL="$_BASE/google-cloud-cli-darwin-x86_64.tar.gz"
+          _SHA_SUM=0c0101a65f3473a5db6a2ebc1867c3294cdbd88fef34da91050869f76149a822
         ;;
         darwin-arm64)
           _URL="$_BASE/google-cloud-cli-darwin-arm.tar.gz"
+          _SHA_SUM=80cfba6da278d0ce8a8a0625bec9648ea5a39ea42daf2536068f19edb175dc70
         ;;
-      esac
+        *)
+           echo "Unsupported for $_NAME: $_OS_NAME-$_MARCH"
+           return 1
+        ;;
+    esac
+
+    # Google Cloud needs special handling: we need a symlink to the source location.
+    pushd /tmp
+    rm -rf google-cloud-sdk
+    _FNAME=google-cloud-sdk.tgz
+    "$SCRIPT_DIR/retry-with-backoff.sh" curl -L -s $_URL -o /tmp/$_FNAME
+    echo "${_SHA_SUM} ${_FNAME}" | sha256sum -c
+    tar xfz $_FNAME
+    popd
+    mkdir -p ${DRIVERS_TOOLS}/.bin
+    ln -s /tmp/google-cloud-sdk/bin/gcloud $DRIVERS_TOOLS/.bin/gcloud
+  ;;
+  *)
+    echo "Unsupported download type $_NAME"
+    return 1
+  ;;
 esac
-
-if [ -z "$_URL" ]; then
-  echo "Unsupported for $_NAME: $_OS_NAME-$_MARCH"
-  return 1
-fi
-
-echo "Installing $_NAME..."
-
-if [ "$_NAME" != "gcloud" ]; then
-  mkdir -p ${DRIVERS_TOOLS}/.bin
-  _TARGET=${DRIVERS_TOOLS}/.bin/$_NAME
-  "$SCRIPT_DIR/retry-with-backoff.sh" curl -L -s $_URL -o $_TARGET
-  chmod +x $_TARGET
-
-else
-  # Google Cloud needs special handling: we need a symlink to the source location.
-  pushd /tmp
-  rm -rf google-cloud-sdk
-  _FNAME=/tmp/google-cloud-sdk.tgz
-  "$SCRIPT_DIR/retry-with-backoff.sh" curl -L -s $_URL -o $_FNAME
-  tar xfz $_FNAME
-  popd
-  mkdir -p ${DRIVERS_TOOLS}/.bin
-  ln -s /tmp/google-cloud-sdk/bin/gcloud $DRIVERS_TOOLS/.bin/gcloud
-fi
-
-echo "Installing $_NAME... done."
