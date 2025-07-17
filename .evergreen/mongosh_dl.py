b45bc4fd543a32e6b04b8137ca220cc717c5dd79
Steven Silvester <steve.silvester@mongodb.com>
2025-07-17 14:02:04 -0500

    use github token

--- .evergreen/mongosh_dl.py
+++ .evergreen/mongosh_dl.py
@@ -39,11 +39,16 @@ def _get_latest_version():
         "Accept": "application/vnd.github+json",
         "X-GitHub-Api-Version": "2022-11-28",
     }
+    if os.environ.get("GITHUB_TOKEN"):
+        headers["authorization"] = f"Bearer {os.environ['GITHUB_TOKEN']}"
     url = "https://api.github.com/repos/mongodb-js/mongosh/releases"
     req = urllib.request.Request(url, headers=headers)
     try:
         resp = urllib.request.urlopen(req, context=SSL_CONTEXT, timeout=30)
     except Exception as e:
+        LOGGER.warning(
+            "Error fetching the latest version, falling back to using git tags"
+        )
         LOGGER.warning(str(e))
         return _get_latest_version_git()
 
