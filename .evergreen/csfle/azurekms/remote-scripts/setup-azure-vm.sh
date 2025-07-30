74bd77d017a4e49a11cd1c0cec8fae4b185a53eb
Steven Silvester <steve.silvester@mongodb.com>
2025-07-30 14:28:56 -0500

    fix VM setup

--- .evergreen/csfle/azurekms/remote-scripts/setup-azure-vm.sh
+++ .evergreen/csfle/azurekms/remote-scripts/setup-azure-vm.sh
@@ -15,5 +15,5 @@ sudo sed -i 's/stable\/updates/stable-security\/updates/' /etc/apt/sources.list
 # Dependencies for mongod: https://www.mongodb.com/docs/manual/tutorial/install-mongodb-enterprise-on-debian-tarball/
 sudo DEBIAN_FRONTEND=noninteractive apt-get $OPTIONS install libcurl4 libgssapi-krb5-2 libldap-common libwrap0 libsasl2-2 libsasl2-modules libsasl2-modules-gssapi-mit openssl liblzma5 < /dev/null > /dev/null
 # Dependencies for drivers-evergreen-tools
-sudo DEBIAN_FRONTEND=noninteractive apt-get $OPTIONS install build-essential python3 python3-venv git < /dev/null > /dev/null
+sudo DEBIAN_FRONTEND=noninteractive apt-get $OPTIONS install python3 python3-venv git < /dev/null > /dev/null
 echo "Installing dependencies ... end"
