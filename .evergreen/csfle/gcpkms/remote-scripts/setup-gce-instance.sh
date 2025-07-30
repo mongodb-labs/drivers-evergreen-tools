a69a14b6a6ee372a46a260d5a91efec7364294d2
Steven Silvester <steve.silvester@mongodb.com>
2025-07-30 16:11:14 -0500

    remove workaround

--- .evergreen/csfle/gcpkms/remote-scripts/setup-gce-instance.sh
+++ .evergreen/csfle/gcpkms/remote-scripts/setup-gce-instance.sh
@@ -10,9 +10,6 @@ echo "set man-db/auto-update false" | sudo debconf-communicate
 sudo dpkg-reconfigure -f noninteractive man-db || true  # This may fail if the lock file is held.
 sudo apt-get -qq update
 OPTIONS="-y -qq -o DPkg::Lock::Timeout=-1"
-# Fix for the error:
-# E: The repository 'http://debian-archive.trafficmanager.net/debian bullseye-backports Release' does not have a Release file
-sudo sed -i 's/stable\/updates/stable-security\/updates/' /etc/apt/sources.list
 # Dependencies for mongod: https://www.mongodb.com/docs/manual/tutorial/install-mongodb-enterprise-on-debian-tarball/
 sudo DEBIAN_FRONTEND=noninteractive apt-get $OPTIONS install libcurl4 libgssapi-krb5-2 libldap-common libwrap0 libsasl2-2 libsasl2-modules libsasl2-modules-gssapi-mit openssl liblzma5 < /dev/null > /dev/null
 # Dependencies for drivers-evergreen-tools
