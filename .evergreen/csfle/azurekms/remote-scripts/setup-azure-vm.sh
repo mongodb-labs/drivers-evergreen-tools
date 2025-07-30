6617c3d3bfdabfd62517bccaa1410a131adc6083
Steven Silvester <steve.silvester@mongodb.com>
2025-07-30 11:03:32 -0500

    fix VM setup

--- .evergreen/csfle/azurekms/remote-scripts/setup-azure-vm.sh
+++ .evergreen/csfle/azurekms/remote-scripts/setup-azure-vm.sh
@@ -9,6 +9,9 @@ echo "set man-db/auto-update false" | sudo debconf-communicate
 sudo dpkg-reconfigure -f noninteractive man-db || true  # This may fail if the lock file is held.
 sudo apt-get -qq update
 OPTIONS="-qq -y -o DPkg::Lock::Timeout=-1"
+# Fix for the error:
+# E: The repository 'http://debian-archive.trafficmanager.net/debian bullseye-backports Release' does not have a Release file
+sudo sed -i 's/stable\/updates/stable-security\/updates/' /etc/apt/sources.list
 # Dependencies for mongod: https://www.mongodb.com/docs/manual/tutorial/install-mongodb-enterprise-on-debian-tarball/
 sudo DEBIAN_FRONTEND=noninteractive apt-get $OPTIONS install libcurl4 libgssapi-krb5-2 libldap-2.4-2 libwrap0 libsasl2-2 libsasl2-modules libsasl2-modules-gssapi-mit snmp openssl liblzma5 < /dev/null > /dev/null
 # Dependencies for drivers-evergreen-tools
