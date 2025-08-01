8628b884d829239c846e1ebf7f5a092442022b07
Kevin Albertson <kevin.albertson@mongodb.com>
2025-08-01 13:53:58 -0400

    override repositories on Debian 11 Azure image

--- .evergreen/csfle/azurekms/remote-scripts/setup-azure-vm.sh
+++ .evergreen/csfle/azurekms/remote-scripts/setup-azure-vm.sh
@@ -3,6 +3,14 @@ set -o errexit
 set -o pipefail
 # Do not error on unset variables. run-orchestration.sh accesses unset variables.
 
+if grep -qs "bullseye" /etc/os-release; then
+    echo "Overwrite repositories to fix DRIVERS-3238 ... begin"
+    echo "deb http://deb.debian.org/debian bullseye main" | sudo tee /etc/apt/sources.list
+    echo "deb http://deb.debian.org/debian-security bullseye-security main" | sudo tee -a /etc/apt/sources.list
+    echo "deb http://deb.debian.org/debian bullseye-updates main" | sudo tee -a /etc/apt/sources.list
+    echo "Overwrite repositories to fix DRIVERS-3238 ... end"
+fi
+
 echo "Installing dependencies ... begin"
 # Skip the "Processing triggers for man-db" step.
 echo "set man-db/auto-update false" | sudo debconf-communicate
