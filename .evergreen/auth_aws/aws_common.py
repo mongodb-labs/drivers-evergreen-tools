769f6476c5a9dae2e7ffb5b2c2b462c2daa439b1
Durran Jordan <durran@gmail.com>
2025-09-17 23:00:48 +0200

    refactor: modules

--- .evergreen/auth_aws/aws_common.py
+++ .evergreen/auth_aws/aws_common.py
@@ -2,7 +2,6 @@
 Script for testing MONGDOB-AWS authentication.
 """
 
-import argparse
 import json
 import logging
 import os
@@ -263,39 +262,3 @@ def setup_eks_pod_identity():
         )
 
     return dict()
-
-
-def main():
-    parser = argparse.ArgumentParser(description="MONGODB-AWS tester.")
-    sub = parser.add_subparsers(title="Tester subcommands", help="sub-command help")
-
-    run_assume_role_cmd = sub.add_parser("assume-role", help="Assume role test")
-    run_assume_role_cmd.set_defaults(func=setup_assume_role)
-
-    run_ec2_cmd = sub.add_parser("ec2", help="EC2 test")
-    run_ec2_cmd.set_defaults(func=setup_ec2)
-
-    run_ecs_cmd = sub.add_parser("ecs", help="ECS test")
-    run_ecs_cmd.set_defaults(func=setup_ecs)
-
-    run_regular_cmd = sub.add_parser("regular", help="Regular credentials test")
-    run_regular_cmd.set_defaults(func=setup_regular)
-
-    run_session_creds_cmd = sub.add_parser("session-creds", help="Session credentials")
-    run_session_creds_cmd.set_defaults(func=setup_session_creds)
-
-    run_env_creds_cmd = sub.add_parser("env-creds", help="Environment credentials")
-    run_env_creds_cmd.set_defaults(func=setup_env_creds)
-
-    run_web_identity_cmd = sub.add_parser("web-identity", help="Web identity test")
-    run_web_identity_cmd.set_defaults(func=setup_web_identity)
-
-    run_eks_pod_identity_cmd = sub.add_parser("eks", help="EKS pod identity test")
-    run_eks_pod_identity_cmd.set_defaults(func=setup_eks_pod_identity)
-
-    args = parser.parse_args()
-    func_name = args.func.__name__.replace("setup_", "").replace("_", "-")
-    LOGGER.info("Running aws_tester.py with %s...", func_name)
-    creds = args.func()
-    handle_creds(creds)
-    LOGGER.info("Running aws_tester.py with %s... done.", func_name)
--- .evergreen/auth_aws/aws_tester.py
+++ .evergreen/auth_aws/aws_tester.py
@@ -3,9 +3,21 @@
 Script for testing MONGDOB-AWS authentication.
 """
 
+import argparse
 from urllib.parse import quote_plus
 
-from aws_common import HERE, main
+from aws_common import (
+    HERE,
+    LOGGER,
+    setup_assume_role,
+    setup_eks_pod_identity,
+    setup_env_creds,
+    setup_ec2,
+    setup_ecs,
+    setup_regular,
+    setup_session_creds,
+    setup_web_identity
+)
 
 
 def handle_creds(creds: dict):
@@ -34,6 +46,42 @@ def handle_creds(creds: dict):
                 value = quote_plus(value)  # noqa: PLW2901
             fid.write(f"export {key}={value}\n")
         fid.write(f'export MONGODB_URI="{MONGODB_URI}"\n')
+
+
+def main():
+    parser = argparse.ArgumentParser(description="MONGODB-AWS tester.")
+    sub = parser.add_subparsers(title="Tester subcommands", help="sub-command help")
+
+    run_assume_role_cmd = sub.add_parser("assume-role", help="Assume role test")
+    run_assume_role_cmd.set_defaults(func=setup_assume_role)
+
+    run_ec2_cmd = sub.add_parser("ec2", help="EC2 test")
+    run_ec2_cmd.set_defaults(func=setup_ec2)
+
+    run_ecs_cmd = sub.add_parser("ecs", help="ECS test")
+    run_ecs_cmd.set_defaults(func=setup_ecs)
+
+    run_regular_cmd = sub.add_parser("regular", help="Regular credentials test")
+    run_regular_cmd.set_defaults(func=setup_regular)
+
+    run_session_creds_cmd = sub.add_parser("session-creds", help="Session credentials")
+    run_session_creds_cmd.set_defaults(func=setup_session_creds)
+
+    run_env_creds_cmd = sub.add_parser("env-creds", help="Environment credentials")
+    run_env_creds_cmd.set_defaults(func=setup_env_creds)
+
+    run_web_identity_cmd = sub.add_parser("web-identity", help="Web identity test")
+    run_web_identity_cmd.set_defaults(func=setup_web_identity)
+
+    run_eks_pod_identity_cmd = sub.add_parser("eks", help="EKS pod identity test")
+    run_eks_pod_identity_cmd.set_defaults(func=setup_eks_pod_identity)
+
+    args = parser.parse_args()
+    func_name = args.func.__name__.replace("setup_", "").replace("_", "-")
+    LOGGER.info("Running aws_tester.py with %s...", func_name)
+    creds = args.func()
+    handle_creds(creds)
+    LOGGER.info("Running aws_tester.py with %s... done.", func_name)
 
 
 if __name__ == "__main__":
