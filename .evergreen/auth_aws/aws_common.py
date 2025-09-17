6d409afae2aecab7a11e25d93f0897bdffb24eb3
Durran Jordan <durran@gmail.com>
2025-09-17 22:42:48 +0200

    refactor: remove handle_creds from common

--- .evergreen/auth_aws/aws_common.py
+++ .evergreen/auth_aws/aws_common.py
@@ -265,34 +265,6 @@ def setup_eks_pod_identity():
     return dict()
 
 
-def handle_creds(creds: dict):
-    if "USER" in creds:
-        USER = quote_plus(creds["USER"])
-        if "PASS" in creds:
-            PASS = quote_plus(creds["PASS"])
-            MONGODB_URI = f"mongodb://{USER}:{PASS}@localhost"
-        else:
-            MONGODB_URI = f"mongodb://{USER}@localhost"
-    elif "MONGODB_URI" in creds:
-        MONGODB_URI = creds.pop("MONGODB_URI")
-    else:
-        MONGODB_URI = "mongodb://localhost"
-    MONGODB_URI = f"{MONGODB_URI}/aws?authMechanism=MONGODB-AWS"
-    if "SESSION_TOKEN" in creds:
-        SESSION_TOKEN = quote_plus(creds["SESSION_TOKEN"])
-        MONGODB_URI = (
-            f"{MONGODB_URI}&authMechanismProperties=AWS_SESSION_TOKEN:{SESSION_TOKEN}"
-        )
-    with (HERE / "test-env.sh").open("w", newline="\n") as fid:
-        fid.write("#!/usr/bin/env bash\n\n")
-        fid.write("set +x\n")
-        for key, value in creds.items():
-            if key in ["USER", "PASS", "SESSION_TOKEN"]:
-                value = quote_plus(value)  # noqa: PLW2901
-            fid.write(f"export {key}={value}\n")
-        fid.write(f'export MONGODB_URI="{MONGODB_URI}"\n')
-
-
 def main():
     parser = argparse.ArgumentParser(description="MONGODB-AWS tester.")
     sub = parser.add_subparsers(title="Tester subcommands", help="sub-command help")
