80fb27810fb8b291695df8e296b1aa2d701ab63a
Durran Jordan <durran@gmail.com>
2025-09-30 14:45:09 +0200

    test: fix

--- .evergreen/tests/test-aws.sh
+++ .evergreen/tests/test-aws.sh
@@ -23,9 +23,6 @@ bash aws_setup.sh --nouri assume-role
 cat test-env.sh | grep -q USER
 cat test-env.sh | grep -q PASS
 cat test-env.sh | grep -q SESSION_TOKEN
-cat test-env.sh | grep -q AWS_ACCESS_KEY_ID
-cat test-env.sh | grep -q AWS_SECRET_ACCESS_KEY
-cat test-env.sh | grep -q AWS_SESSION_TOKEN
 # Ensure there is no password in the URI.
 cat test-env.sh | grep MONGODB_URI | grep -v -q "@"
 rm test-env.sh
@@ -46,9 +43,6 @@ bash aws_setup.sh --nouri regular
 cat test-env.sh | grep -q USER
 cat test-env.sh | grep -q PASS
 cat test-env.sh | grep -q SESSION_TOKEN
-cat test-env.sh | grep -q AWS_ACCESS_KEY_ID
-cat test-env.sh | grep -q AWS_SECRET_ACCESS_KEY
-cat test-env.sh | grep -q AWS_SESSION_TOKEN
 # Ensure there is no password in the URI.
 cat test-env.sh | grep MONGODB_URI | grep -v -q "@"
 rm test-env.sh
