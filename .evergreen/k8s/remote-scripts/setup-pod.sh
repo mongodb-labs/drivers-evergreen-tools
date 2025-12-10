b83cd63f00a7a82a5d258435637afb409a886359
Steven Silvester <steve.silvester@mongodb.com>
2025-12-10 06:09:07 -0600

    fix pod setup

--- .evergreen/k8s/remote-scripts/setup-pod.sh
+++ .evergreen/k8s/remote-scripts/setup-pod.sh
@@ -2,7 +2,10 @@
 set -eu
 
 echo "Installing dependencies ... begin"
-apt-get -qq update
+# Skip the "Processing triggers for man-db" step.
+echo "set man-db/auto-update false" | sudo debconf-communicate
+sudo dpkg-reconfigure -f noninteractive man-db || true  # This may fail if the lock file is held.
+apt-get -qq -o DPkg::Lock::Timeout=-1 update
 # Same dependencies used in KMS testing.
 export DEBIAN_FRONTEND=noninteractive
 apt-get -qq -y -o DPkg::Lock::Timeout=-1 install libcurl4 libgssapi-krb5-2 libldap-common libwrap0 libsasl2-2 \
