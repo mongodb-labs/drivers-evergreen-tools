8c1401c9c2f1ab07c76e979dc679267df5a1224f
Steven Silvester <steve.silvester@mongodb.com>
2025-12-22 09:46:56 -0600

    DRIVERS-3035 Move MongoDB 3.6 and 4.0 to legacy support

---
+++ .evergreen/tests/test-legacy-versions.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Test mongodl and mongosh_dl.
+set -eu
+
+SCRIPT_DIR=$(dirname ${BASH_SOURCE[0]})
+. $SCRIPT_DIR/../handle-paths.sh
+
+pushd $DRIVERS_TOOLS/.evergreen > /dev/null
+
+for v in 3.6 4.0; do
+  URI=$(bash run-orchestration.sh --version $v)
+  $MONGODB_BINARIES/mongosh $URI --eval "db.runCommand({\"ping\":1})"
+  bash stop-orchesration.sh
+done
+
+popd
+make -C ${DRIVERS_TOOLS} test
