bca8276d19110de326bfdc1d9cf4965ccafc5c42
Steven Silvester <steve.silvester@mongodb.com>
2025-12-22 10:15:30 -0600

    fix usage

--- .evergreen/tests/test-legacy-versions.sh
+++ .evergreen/tests/test-legacy-versions.sh
@@ -6,13 +6,13 @@ set -eu
 SCRIPT_DIR=$(dirname ${BASH_SOURCE[0]})
 . $SCRIPT_DIR/../handle-paths.sh
 
-pushd $DRIVERS_TOOLS/.evergreen
+pushd $DRIVERS_TOOLS
 
 for v in 3.6 4.0; do
   export MONGODB_VERSION=$v
-  TOPOLOGY=standalone bash run-tests.sh
-  TOPOLOGY=replica_set SSL=ssl bash run-tests.sh
-  TOPOLOGY=sharded_cluster AUTH=auth SSL=ssl bash run-tests.sh
+  TOPOLOGY=standalone bash ./.evergreen/run-tests.sh
+  TOPOLOGY=replica_set SSL=ssl bash ./.evergreen/run-tests.sh
+  TOPOLOGY=sharded_cluster AUTH=auth SSL=ssl bash ./.evergreen/run-tests.sh
 done
 
 popd
