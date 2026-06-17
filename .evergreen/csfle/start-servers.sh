7541d3ff7096399216c6c4d12156e29d1891cd44
Preston Vasquez <prestonvasquez@icloud.com>
2026-06-17 15:12:24 -0600

    DRIVERS-3550: start-servers.sh does not reap a stale KMS failpoint server on port 9003 (#792)

--- .evergreen/csfle/start-servers.sh
+++ .evergreen/csfle/start-servers.sh
@@ -23,7 +23,7 @@ fi
 # Forcibly kill the process listening on the desired ports, most likely
 # left running from a previous task.
 . "$SCRIPT_DIR/../process-utils.sh"
-for port in 5698 9000 9001 9002 9004 9005 8080; do
+for port in 5698 9000 9001 9002 9003 9004 9005 8080; do
   killport $port 9
 done
 
