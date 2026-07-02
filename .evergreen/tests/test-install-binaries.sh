327c6fb2f864eb49f992df7858c04bb898c85d09
Steven Silvester <steve.silvester@mongodb.com>
2026-07-02 07:39:30 -0500

    DRIVERS-3558: fix Windows path handling in mongodb-runner test check

--- .evergreen/tests/test-install-binaries.sh
+++ .evergreen/tests/test-install-binaries.sh
@@ -24,7 +24,7 @@ import sys
 sys.path.insert(0, 'orchestration')
 from mongodb_runner import _install_mongodb_runner, _normalize_path
 print(_normalize_path(_install_mongodb_runner()))
-")
+" | tr -d '\r')
   "$RUNNER_BIN" --help
 else
   echo "mongodb-runner is not supported on this platform; skipping check"
